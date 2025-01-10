35a6b4938b44c165a5bf2476c852511ff5a2ae1d
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-10 15:46:54 +0000

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,36 @@
 import { auth } from "@/auth";
-import { authRoutes, publicRoutes } from "./routes";
+import { authRoutes, protectedRoutes, publicRoutes } from "./routes";
 import { NextResponse } from "next/server";
 
 export default auth((req) => {
-  const {nextUrl} = req;
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
 
   const isPublic = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  const isProtected = protectedRoutes.includes(nextUrl.pathname);
 
   if (isPublic) {
     return NextResponse.next();
   }
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return NextResponse.redirect(new URL('/profile', nextUrl));
+  if (isProtected) {
+    if (!isLoggedIn) {
+      return NextResponse.redirect(new URL("/login", nextUrl));
     } else {
       return NextResponse.next();
     }
   }
 
-  if (!isPublic && !isLoggedIn) {
-    return NextResponse.redirect(new URL('/login', nextUrl));
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL("/profile", nextUrl))
+    } else {
+      return
+    }
   }
-})
+
+});
 
 export const config = {
   matcher: [
