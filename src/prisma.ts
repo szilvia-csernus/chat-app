a600895aa3f6c71ce52658e687b10930db70e93b
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-10 15:48:06 +0000

    add comment to prisma client initialisation

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,7 @@
+// this code makes sure that the prisma client is instantiated only once
+// and that it is reused across hot reloads
+// code source: https://authjs.dev/getting-started/adapters/prisma
+
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
