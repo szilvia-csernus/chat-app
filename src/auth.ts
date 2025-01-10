65057b42ef82c8a1606ac797167aaae6ad278251
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-10 15:46:28 +0000

    update auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,111 +1,65 @@
-import NextAuth from "next-auth";
+import NextAuth, { Account, Session, User } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+
 import { prisma } from "@/prisma";
 import Credentials from "next-auth/providers/credentials";
-import { loginSchema } from "./lib/schemas/loginSchema";
-import { getUserByEmail } from "./app/actions/authActions";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
+import Linkedin from "next-auth/providers/linkedin";
+import { loginCredentialsSchema } from "./lib/schemas/loginCredentialsSchema";
+import { getUserByEmail, getUserCredentials } from "./app/actions/authActions";
 import { compare } from "bcryptjs";
+// import Passkey from "next-auth/providers/passkey";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  // experimental: { enableWebAuthn: true },
   providers: [
-    Credentials({
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      credentials: {
-        email: {},
-        passwordHash: {},
-      },
-      authorize: async (credentials) => {
-        let user = null;
-
-        const validated = loginSchema.safeParse(credentials);
-
-        if (validated.success) {
-          const { email, password } = validated.data;
-
-          user = await getUserByEmail(email);
-
-          if (
-            !user ||
-            !user.passwordHash ||
-            !(await compare(password, user.passwordHash))
-          )
-            return null;
-
-          return user;
-        } else {
-          return null;
-        }
-      },
+    // Passkey,
+    Google({
+      allowDangerousEmailAccountLinking: true
+    }),
+    Github({
+      allowDangerousEmailAccountLinking: true
+    }),
+    Linkedin({
+      allowDangerousEmailAccountLinking: true
     }),
   ],
   session: {
     strategy: "jwt",
   },
   callbacks: {
-    async jwt(jwt_token) {
-      console.log("jwt token", jwt_token);
-
-      // jwt token {
-      //   token: {
-      //     token: {
-      //       name: 'Joe',
-      //       email: 'joe@test.com',
-      //       picture: null,
-      //       sub: 'cm1822stl000148nq2ybj6ny5'
-      //     },
-      //     user: {
-      //       id: 'cm1822stl000148nq2ybj6ny5',
-      //       name: 'Joe',
-      //       email: 'joe@test.com',
-      //       emailVerified: null,
-      //       passwordHash: '$2a$10$AcpWFUraEFVsuNMTg2RTKO1seN4MhDKawyKJLF4ODkG',
-      //       image: null,
-      //       createdAt: '2024-09-18T16:06:51.753Z',
-      //       updatedAt: '2024-09-18T16:06:51.753Z'
-      //     },
-      //     account: {
-      //       providerAccountId: 'cm1822stl000148nq2ybj6ny5',
-      //       type: 'credentials',
-      //       provider: 'credentials'
-      //     },
-      //     isNewUser: false,
-      //     trigger: 'signIn',
-      //     iat: 1726735785,
-      //     exp: 1729327785,
-      //     jti: '2dde5464-8c68-4884-b8b5-188cd4336a7f'
-      //   },
-      //   session: undefined
-      // }
-
+    async jwt(jwt_token: {
+      token: JWT;
+      user?: User;
+      account?: Account | null;
+      profile?: any;
+    }) {
+      if (jwt_token.user) {
+        jwt_token.token.sub = jwt_token.user.id;
+        jwt_token.token.provider = jwt_token.account?.provider;
+        jwt_token.token.picture =
+          jwt_token.profile?.picture || jwt_token.user.image;
+        jwt_token.token.name = jwt_token.profile?.name;
+      }
+      // console.log("jwt token", jwt_token.token);
       // return value will be passed to the session callback as 'token'.
-      // if no jwt callback is defined, the whole jwt_token will be passed to the
+      // if no jwt callback is defined, only a safe subset of the jwt_token will be passed to the
       // session callback as 'token'. By passing the jwt_token.token to the
       // session callback, it will populate the user data in the session object.
-      return jwt_token.token ? jwt_token.token : null;
+      return jwt_token.token;
     },
-    async session(session_token) {
-      console.log("session", session_token);
-
+    async session({ session, token }: { session: Session; token: JWT }) {
+      session.user.id = token.sub;
+      session.user.provider = token.provider;
+      session.user.image = token.picture;
+      session.user.name = token.name;
+      console.log("session token user", session);
       // return value will be passed to the client side -- be careful with sensitive data!!
-      return session_token.session;
+
+      return session;
     },
-    // async session(session_token) {
-    //   token consists of whatever the jwt callback returns
-    //   const { session, token } = session_token;
-
-    //   console.log("session_token", session_token);
-    //   console.log("session", session);
-    //   console.log("token", token)
-
-    //   if (token.sub && session.user) {
-    //     session.user.id = token.sub;
-    //   }
-
-    //   console.log("user", session.user);
-
-    //   return session.user;
-    // },
   },
 });
