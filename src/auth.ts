1003f0843f6f423a10ac94e30d6c58539b2aafc3
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-14 10:33:55 +0000

    update next-auth files

--- src/auth.ts
+++ src/auth.ts
@@ -1,65 +1,12 @@
-import NextAuth, { Account, Session, User } from "next-auth";
-import { JWT } from "next-auth/jwt";
+import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-
 import { prisma } from "@/prisma";
-import Credentials from "next-auth/providers/credentials";
-import Google from "next-auth/providers/google";
-import Github from "next-auth/providers/github";
-import Linkedin from "next-auth/providers/linkedin";
-import { loginCredentialsSchema } from "./lib/schemas/loginCredentialsSchema";
-import { getUserByEmail, getUserCredentials } from "./app/actions/authActions";
-import { compare } from "bcryptjs";
-// import Passkey from "next-auth/providers/passkey";
+import authConfig from "@/auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  // experimental: { enableWebAuthn: true },
-  providers: [
-    // Passkey,
-    Google({
-      allowDangerousEmailAccountLinking: true
-    }),
-    Github({
-      allowDangerousEmailAccountLinking: true
-    }),
-    Linkedin({
-      allowDangerousEmailAccountLinking: true
-    }),
-  ],
   session: {
     strategy: "jwt",
   },
-  callbacks: {
-    async jwt(jwt_token: {
-      token: JWT;
-      user?: User;
-      account?: Account | null;
-      profile?: any;
-    }) {
-      if (jwt_token.user) {
-        jwt_token.token.sub = jwt_token.user.id;
-        jwt_token.token.provider = jwt_token.account?.provider;
-        jwt_token.token.picture =
-          jwt_token.profile?.picture || jwt_token.user.image;
-        jwt_token.token.name = jwt_token.profile?.name;
-      }
-      // console.log("jwt token", jwt_token.token);
-      // return value will be passed to the session callback as 'token'.
-      // if no jwt callback is defined, only a safe subset of the jwt_token will be passed to the
-      // session callback as 'token'. By passing the jwt_token.token to the
-      // session callback, it will populate the user data in the session object.
-      return jwt_token.token;
-    },
-    async session({ session, token }: { session: Session; token: JWT }) {
-      session.user.id = token.sub;
-      session.user.provider = token.provider;
-      session.user.image = token.picture;
-      session.user.name = token.name;
-      console.log("session token user", session);
-      // return value will be passed to the client side -- be careful with sensitive data!!
-
-      return session;
-    },
-  },
+  ...authConfig,
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,46 +1,4 @@
-import { auth } from "@/auth";
-import { authRoutes, protectedRoutes, publicRoutes } from "./routes";
-import { NextResponse } from "next/server";
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
 
-export default auth((req) => {
-  const { nextUrl } = req;
-  const isLoggedIn = !!req.auth;
-
-  const isPublic = publicRoutes.includes(nextUrl.pathname);
-  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  const isProtected = protectedRoutes.includes(nextUrl.pathname);
-
-  if (isPublic) {
-    return NextResponse.next();
-  }
-
-  if (isProtected) {
-    if (!isLoggedIn) {
-      return NextResponse.redirect(new URL("/login", nextUrl));
-    } else {
-      return NextResponse.next();
-    }
-  }
-
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return NextResponse.redirect(new URL("/profile", nextUrl))
-    } else {
-      return
-    }
-  }
-
-});
-
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
-  ],
-};
+export const { auth: middleware } = NextAuth(authConfig);
