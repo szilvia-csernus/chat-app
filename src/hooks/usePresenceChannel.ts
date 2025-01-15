1c5188c63884ced4b54cfb19d6cee1efca38fa68
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-15 12:15:31 +0000

    add PresenceStore and PresenceChannel functions

---
+++ src/hooks/usePresenceChannel.ts
@@ -0,0 +1,76 @@
+"use client";
+
+import { useCallback, useEffect, useRef } from "react";
+import usePresenceStore from "./usePresenceStore";
+import { Channel, Members } from "pusher-js";
+import { pusherClient } from "@/lib/pusher";
+
+export const usePresenceChannel = (userId: string | null) => {
+  const set = usePresenceStore((state) => state.set);
+  const add = usePresenceStore((state) => state.add);
+  const remove = usePresenceStore((state) => state.remove);
+
+  // this is used to prevent the creation of multiple channels when the component re-renders
+  const channelRef = useRef<Channel | null>(null);
+
+  const handleSetMembers = useCallback(
+    (memberIds: string[]) => {
+      set(memberIds);
+    },
+    [set]
+  );
+
+  const handleAddMember = useCallback(
+    (memberId: string) => {
+      add(memberId);
+    },
+    [add]
+  );
+
+  const handleRemoveMember = useCallback(
+    (memberId: string) => {
+      remove(memberId);
+    },
+    [remove]
+  );
+
+  useEffect(() => {
+    if (!userId) return;
+    if (!channelRef.current) {
+      channelRef.current = pusherClient.subscribe("presence-chat");
+
+      channelRef.current.bind(
+        "pusher:subscription_succeeded",
+        (members: Members) => {
+          handleSetMembers(Object.keys(members.members));
+        }
+      );
+
+      channelRef.current.bind(
+        "pusher:member_added",
+        (member: Record<string, never>) => {
+          handleAddMember(member.id);
+        }
+      );
+
+      channelRef.current.bind(
+        "pusher:member_removed",
+        (member: Record<string, never>) => {
+          handleRemoveMember(member.id);
+        }
+      );
+    }
+
+    return () => {
+      if (channelRef.current && channelRef.current.subscribed) {
+        channelRef.current.unsubscribe();
+        channelRef.current.unbind(
+          "pusher:subscription_succeeded",
+          handleSetMembers
+        );
+        channelRef.current.unbind("pusher:member_added", handleAddMember);
+        channelRef.current.unbind("pusher:member_removed", handleRemoveMember);
+      }
+    };
+  }, [handleSetMembers, handleAddMember, handleRemoveMember, userId]);
+};
