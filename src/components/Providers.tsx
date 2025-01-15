b067b518c1a4df3a3f3f0d4d4c6483af194ff296
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-15 12:15:49 +0000

    add PresenceChannel to Provider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,19 @@
 "use client"
 
+import { usePresenceChannel } from '@/hooks/usePresenceChannel';
 import { NextUIProvider } from '@nextui-org/react'
 import React, { ReactNode } from 'react'
 import { ToastContainer} from 'react-toastify'
 import "react-toastify/ReactToastify.min.css";
 
-export default function Providers({children}: {children: ReactNode}) {
+type ProvidersProps = {
+  children: ReactNode;
+  userId: string;
+};
+
+export default function Providers({children, userId}: ProvidersProps) {
+  usePresenceChannel(userId);
+
   return (
     <NextUIProvider>
       <ToastContainer position='bottom-right' hideProgressBar className='z-4' />
