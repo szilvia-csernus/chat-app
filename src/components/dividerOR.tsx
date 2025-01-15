a7cacf671fedaa69b9a3870df706161a527f4e71
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-15 12:13:20 +0000

    update divider markup

--- src/components/dividerOR.tsx
+++ src/components/dividerOR.tsx
@@ -2,14 +2,10 @@ import React from 'react'
 
 export default function DividerOR() {
   return (
-    <div className="flex justify-around ">
-      <div className="w-[40%] border-b-1 my-4 border-gray-400">
-        <hr />
-      </div>
-      <p className="text-center px-4 mt-[7px] text-sm">OR</p>
-      <div className="w-[40%] border-b-1 my-4 border-gray-400">
-        <hr />
-      </div>
+    <div className="flex w-full justify-between">
+      <div className="w-[42%] border-b-1 my-4 border-[#999]"></div>
+      <p className="text-center px-4 mt-[7px] text-[#999] text-sm">OR</p>
+      <div className="w-[42%] border-b-1 my-4 border-[#999]"></div>
     </div>
   );
 }
