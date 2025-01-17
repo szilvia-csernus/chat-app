0db77852fad13ae2aced2702812e69636ef31c65
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-17 17:54:42 +0000

    add ChatPartnersStore to zustand store

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,11 @@ import { auth } from "@/auth";
 import { getUserById } from "./actions/authActions";
 import { getPhotoByUserId } from "./actions/photoActions";
 import { User } from "@prisma/client";
-
+import {
+  getChatPartners,
+  getRecentChats,
+} from "./actions/conversationActions";
+import { getCurrentProfile } from "./actions/memberActions";
 
 export const metadata: Metadata = {
   title: "Chat App",
@@ -31,11 +35,61 @@ export default async function RootLayout({
   }
   const photoUrl = userPhoto ? userPhoto.imageUrl : "/images/user.png";
 
+  const currentProfile = await getCurrentProfile();
+
+  const chatPartnersData = await getChatPartners();
+  const chatPartners = (chatPartnersData || []).map((cp) => {
+    if (cp.profile1.id === currentProfile?.id) {
+      return {
+        chatId: cp.id,
+        chatPartner: {
+          id: cp.profile2.id,
+          name: cp.profile2.user.name || "",
+          image: cp.profile2.user.image || "",
+        },
+        participant1: false
+      };
+    } else {
+      return {
+        chatId: cp.id,
+        chatPartner: {
+          id: cp.profile1.id,
+          name: cp.profile1.user.name || "",
+          image: cp.profile1.user.image || "",
+        },
+        participant1: true
+      };
+    }
+  });
+
+  const recentChatsData = await getRecentChats();
+  const recentChats = (recentChatsData || []).map((chat) => {
+    return {
+      id: chat.id,
+      participant1: {
+        id: chat.profile1.id,
+        name: chat.profile1.user.name || "",
+        image: chat.profile1.user.image || "",
+      },
+      participant2: {
+        id: chat.profile2.id,
+        name: chat.profile2.user.name || "",
+        image: chat.profile2.user.image || "",
+      },
+      lastMessage: chat.messages[0]?.content || "",
+      unreadMessages: chat._count.messages,
+    };
+  });
+
   return (
     <html lang="en">
       <body className="font-body h-screen">
-        <Providers userId={session?.user?.id}>
-          <MainNav user={user} photoUrl={photoUrl}/>
+        <Providers
+          userId={session?.user?.id}
+          recentChats={recentChats}
+          chatPartners={chatPartners}
+        >
+          <MainNav user={user} photoUrl={photoUrl} />
           <main className="container mx-auto p-10">{children}</main>
         </Providers>
       </body>
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,23 +1,41 @@
-"use client"
+"use client";
 
-import { usePresenceChannel } from '@/hooks/usePresenceChannel';
-import { NextUIProvider } from '@nextui-org/react'
-import React, { ReactNode } from 'react'
-import { ToastContainer} from 'react-toastify'
+import { useRecentChatsStore } from "@/hooks/useRecentChatsStore";
+import { usePresenceChannel } from "@/hooks/usePresenceChannel";
+import { useChatPartnersStore } from "@/hooks/useChatPartnersStore";
+import { ChatPartner, RecentChat } from "@/types";
+import { NextUIProvider } from "@nextui-org/react";
+import React, { ReactNode } from "react";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/ReactToastify.min.css";
 
 type ProvidersProps = {
   children: ReactNode;
   userId: string;
+  recentChats: RecentChat[] | null;
+  chatPartners: ChatPartner[] | null;
 };
 
-export default function Providers({children, userId}: ProvidersProps) {
+export default function Providers({
+  children,
+  userId,
+  recentChats,
+  chatPartners,
+}: ProvidersProps) {
   usePresenceChannel(userId);
+  const { set: setRecentChats } = useRecentChatsStore();
+  if (recentChats) {
+    setRecentChats(recentChats);
+  }
+  const { set: setChatPartners } = useChatPartnersStore();
+  if (chatPartners) {
+    setChatPartners(chatPartners);
+  }
 
   return (
     <NextUIProvider>
-      <ToastContainer position='bottom-right' hideProgressBar className='z-4' />
+      <ToastContainer position="bottom-right" hideProgressBar className="z-4" />
       {children}
     </NextUIProvider>
-  )
+  );
 }
