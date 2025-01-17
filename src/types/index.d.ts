2f4eda89cfad7a2957ba9f436a84bd5e6415173f
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-17 17:51:00 +0000

    add custom types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,3 +3,29 @@ import { ZodIssue } from "zod";
 type ActionResult<T> =
   | { status: "success"; data: T }
   | { status: "error"; error: string | ZodIssue[] };
+
+type Member = {
+  id: string;
+  name: string;
+  image: string;
+}
+
+type Chat = {
+  id: string;
+  participant1: Member;
+  participant2: Member;
+  messages: Message[];
+}
+
+type ChatPartner = {
+  chatPartner: Member;
+  chatId: string;
+}
+
+type RecentChat = {
+  id: string;
+  participant1: Member;
+  participant2: Member;
+  lastMessage: string;
+  unreadMessages: number;
+}
