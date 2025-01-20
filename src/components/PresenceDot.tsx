4a980051b89e2ebf6196448083d64f518ae67d78
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-20 17:43:20 +0000

    adjust size of Presence Dot

--- src/components/PresenceDot.tsx
+++ src/components/PresenceDot.tsx
@@ -10,9 +10,9 @@ export default function PresenceDot({ outlineColor="primary" }: PresenceDotProps
 
   return (
     <span>
-      <GoDot size={26} className={`fill-${outlineColor} absolute left-[-3px] top-0`}/>
+      <GoDot size={28} className={`fill-${outlineColor} absolute left-[-3px] top-0`}/>
       <GoDotFill
-        size={20}
+        size={22}
         className="fill-teal-400 animate-pulse absolute top-[3px] left-0"
       />
     </span>
