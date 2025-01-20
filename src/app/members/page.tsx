ba65e7013868dd99f072f04c2f3a72409a4fc4ab
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-20 17:44:45 +0000

    update members' page

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,9 +1,17 @@
 import { getMembers } from '@/app/actions/memberActions';
 import MembersList from './MembersList';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 
 export const dynamic = "force-dynamic";
 
 export default async function MembersPage() {
+  const session = await auth();
+
+  if (!session) {
+    return redirect('/login');
+  }
+
   const membersData = await getMembers();
   const members = membersData ? membersData.map(member => ({
     id: member.id,
