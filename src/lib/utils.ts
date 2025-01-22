bf0322cc6a8705b6c5445a9bf365a8f51b102e91
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-22 15:54:45 +0000

    refactor and update ChatPartner data fetching

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
+import { ChatPartner, CPData } from "@/types";
+import { Conversation, Profile } from "@prisma/client";
+import { format, formatDistanceToNow } from "date-fns";
+
 export const calculateAge = (dateString: string) => {
   const today = new Date();
   const birthDate = new Date(dateString);
@@ -9,4 +13,42 @@ export const calculateAge = (dateString: string) => {
   }
 
   return age;
+}
+
+export function formatShortDateTime(date: Date) {
+  return format(date, "dd MMM yy h:mm:a");
+}
+
+export function timeAgo(date: string) {
+  try {
+    return formatDistanceToNow(new Date(date), { addSuffix: true });
+  } catch (error) {
+    return date;
+  }
+}
+
+export function mapCPDataToChatPartnerType(currentProfile: Profile | null, chatPartnersData: CPData[] | null): ChatPartner[] {
+  if (!currentProfile || !chatPartnersData) return [];
+
+  return chatPartnersData.map(cp => {
+    if (cp.profile1.id === currentProfile?.id) {
+      return {
+        chatId: cp.id,
+        chatPartner: {
+          id: cp.profile2.id,
+          name: cp.profile2.user.name || "",
+          image: cp.profile2.user.image || "",
+        },
+      };
+    } else {
+      return {
+        chatId: cp.id,
+        chatPartner: {
+          id: cp.profile1.id,
+          name: cp.profile1.user.name || "",
+          image: cp.profile1.user.image || "",
+        },
+      };
+    }
+  });
 }
