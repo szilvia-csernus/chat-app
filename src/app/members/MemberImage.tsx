d98c40a62b4b991dddfc9c1f7d8bf4581dc9eb51
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-22 10:47:59 +0000

    update MemberImage

--- src/app/members/MemberImage.tsx
+++ src/app/members/MemberImage.tsx
@@ -10,33 +10,36 @@ type MemberImageProps = {
   className?: string;
 };
 
-export default function MemberImage({ memberName, memberImage, width, height, className }: MemberImageProps) {
+const MemberImage: React.FC<MemberImageProps> = ({ memberName, memberImage, width, height, className }) => {
   
-  let image = null
+  let image: JSX.Element | null = null;
 
   if (memberImage && memberImage.includes('cloudinary')) {
-    image = (
-      <CldImage
-        alt="Image of member"
-        src={memberImage}
-        width={width || 300}
-        height={height || 300}
-        className={className || "aspect-square object-cover"}
-        crop="fill"
-        gravity="faces"
-      />
-    ); // faces is a Cloudinary transformation, makes sure that the faces are not cropped out
+  const cloudinaryUrl = `${memberImage}?cache-control=max-age=31536000`;
+  image = (
+    <CldImage
+    alt={memberName || "Image of member"}
+    src={cloudinaryUrl}
+    width={width || 300}
+    height={height || 300}
+    className={className || "aspect-square object-cover"}
+    crop="fill"
+    gravity="faces"
+    />
+  ); // faces is a Cloudinary transformation, makes sure that the faces are not cropped out
   } else {
-    image = (
-      <Image
-        isZoomed
-        alt={memberName || "user"}
-        width={width || 300}
-        height={height || 300}
-        src={memberImage || "/images/user.png"}
-        className={className || "aspect-square object-cover"}
-      />
-    );
+  image = (
+    <Image
+      isZoomed
+      alt={memberName || "Image of member"}
+      width={width || 300}
+      height={height || 300}
+      src={memberImage || "/images/user.png"}
+      className={className || "aspect-square object-cover"}
+    />
+  );
   }
   return <>{image}</>;
 }
+
+export default React.memo(MemberImage);
