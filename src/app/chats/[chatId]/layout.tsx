fba503f4c211da5ba4896855d6523f9c432ee2f3
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-23 09:52:51 +0000

    update Chat layout

--- src/app/chats/[chatId]/layout.tsx
+++ src/app/chats/[chatId]/layout.tsx
@@ -3,6 +3,7 @@ import { notFound, redirect } from "next/navigation";
 import { Card } from "@heroui/card";
 import { auth } from "@/auth";
 import Sidebar from "./(sidebar)/Sidebar";
+import { getCurrentProfile } from "@/app/actions/memberActions";
 
 export default async function ChatLayout({
   params,
@@ -18,12 +19,14 @@ export default async function ChatLayout({
   }
 
   const { chatId } = params;
+  const currentProfile = await getCurrentProfile();
+  if (!currentProfile) return redirect("/complete-profile");
 
   return (
     <>
       <div className="grid grid-cols-12 gap-2">
         <div className="col-span-4 lg:col-span-4">
-          <Sidebar chatId={chatId} />
+          <Sidebar currentChatId={chatId} currentProfile={currentProfile} />
         </div>
         <div className="col-span-8 lg:col-span-8">
           <Card className="w-full h-[85vh] p-5 overflow-auto py-8">
