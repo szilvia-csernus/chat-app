864dfa340af7ec86a541267f6234fa72a12ef24b
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-24 16:36:02 +0000

    update [chatId] page

--- src/app/chats/[chatId]/page.tsx
+++ src/app/chats/[chatId]/page.tsx
@@ -1,33 +1,34 @@
-import { notFound } from "next/navigation";
-// import ChatForm from "./ChatForm";
-import { getCurrentUserId } from "@/app/actions/authActions";
-// import MessageThread from "./MessageThread";
+import { notFound, redirect } from "next/navigation";
+import ChatForm from "./(main)/ChatForm";
 import { getChat } from "@/app/actions/chatActions";
-import { getCurrentProfile, getMemberById } from "@/app/actions/memberActions";
-
+import ChatThread from "./(main)/ChatThread";
+import { mapChatDataToChatType } from "@/lib/utils";
+import { getCurrentMember, getMemberById } from "@/app/actions/memberActions";
 
 export default async function Chat({ params }: { params: { chatId: string } }) {
-  const initialThreadMessages = await getChat(params.chatId);
-  if (!initialThreadMessages) return notFound();
+  const chat = await getChat(params.chatId);
+  if (!chat) return notFound();
+  const currentChat = mapChatDataToChatType(chat);
 
-  const currentProfile = await getCurrentProfile();
-  
-  const chatPartner = await getMemberById(
-    initialThreadMessages.profile1Id === currentProfile?.id
-      ? initialThreadMessages.profile2Id
-      : initialThreadMessages.profile1Id
-  );
+  const currentMember = await getCurrentMember();
+  if (!currentMember) return redirect("/complete-profile");
+  const chatPartnerId =
+    chat.profile1Id === currentMember.id ? chat.profile2Id : chat.profile1Id;
+
+  const chatPartner = await getMemberById(chatPartnerId);
+  if (!chatPartner) return notFound();
 
   return (
-    <div className="flex flex-col gap-4">
-      {/* <ChatThread
-        chatThread={chatThread}
-      /> */}
-      {/* {chatPartner && (
-        <div className="flex items-center space-x-2">
-          <ChatForm otherPersonId={chatPartner.id} />
-        </div>
-      )} */}
+    <div className="flex flex-col justify-between h-full">
+      <ChatThread
+        currentChat={currentChat}
+        currentMember={currentMember}
+        chatPartner={chatPartner}
+      />
+
+      <div className="flex items-center space-x-2">
+        <ChatForm chatPartnerId={chatPartnerId} />
+      </div>
     </div>
   );
 }
