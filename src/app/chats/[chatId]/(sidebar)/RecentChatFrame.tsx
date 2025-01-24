a0cb7a22054d99d48bb37925cc51a74149a33d90
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-24 16:34:20 +0000

    update Sidebar

--- src/app/chats/[chatId]/(sidebar)/RecentChatFrame.tsx
+++ src/app/chats/[chatId]/(sidebar)/RecentChatFrame.tsx
@@ -1,30 +1,30 @@
+"use client";
+
 import Link from "next/link";
+import { useParams } from "next/navigation";
 import React from "react";
 
 type Props = {
   chatId: string;
   children: React.ReactNode;
-  currentChatId: string;
 };
 
 
 export default function RecentChatFrame({
   chatId,
   children,
-  currentChatId,
 }: Props) {
 
-  console.log("currentChatId", currentChatId);
-  console.log("chatId", chatId);
+  const params = useParams();
+  const currentChatId = params.chatId;
 
   const additionalStyles = currentChatId === chatId
-      ? "border-1 border-secondary rounded-lg"
+      ? "border-1 border-slate-400 rounded-2xl bg-white"
       : "";
 
   return (
     <Link
-      key={chatId}
-      className={`relative flex flex-row items-center gap-2 w-full cursor-pointer ${additionalStyles}`}
+      className={`relative flex flex-row items-center w-full cursor-pointer ${additionalStyles}`}
       href={`/chats/${chatId}`}
     >
       {children}
