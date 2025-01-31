4f12e73809b3a567287ebb90864bd3493a3145d5
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-31 18:06:25 +0000

    update main chat thread and its components

--- src/app/chats/[chatId]/(main)/ChatThread.tsx
+++ src/app/chats/[chatId]/(main)/ChatThread.tsx
@@ -1,110 +1,41 @@
 "use client";
 
-import { useCurrentChatStore } from "@/hooks/useCurrentChatStore";
+import { useCurrentChatStore } from "@/hooks/zustand-stores/useCurrentChatStore";
 import { Chat, Member } from "@/types";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect } from "react";
 import MessageBox from "./MessageBox";
-import { pusherClient } from "@/lib/pusher";
-import { formatShortDateTime } from "@/lib/utils";
-import { Channel } from "pusher-js";
-import usePresenceStore from "@/hooks/usePresenceStore";
-import { Message } from "@prisma/client";
-import { useRecentChatsStore } from "@/hooks/useRecentChatsStore";
-import { useRouter } from "next/navigation";
-import { updateReadStatus } from "@/app/actions/messageActions";
+
+import usePresenceStore from "@/hooks/zustand-stores/usePresenceStore";
+import { useRecentChatsStore } from "@/hooks/zustand-stores/useRecentChatsStore";
 
 type Props = {
-  initialChat: Chat | null;
   currentMember: Member;
   chatPartner: Member;
+  initialChat: Chat | null;
 };
 
-export default function ChatThread({
-  initialChat,
-  currentMember,
-  chatPartner,
-}: Props) {
-  const setCurrentChat = useCurrentChatStore((state) => state.setCurrentChat);
+export default function ChatThread({ currentMember, chatPartner, initialChat }: Props) {
   const currentChat = useCurrentChatStore((state) => state.chat);
-  const addMessage = useCurrentChatStore((state) => state.addMessage);
-  const updateMessageReadStatus = useCurrentChatStore(
-    (state) => state.updateMessageReadStatus
-  );
-
-  // Pusher channel reference
-  const channelRef = useRef<Channel | null>(null);
-
   const membersOnline = usePresenceStore((state) => state.membersOnline);
-
-  const updateUnreadCount = useRecentChatsStore(
-    (state) => state.updateUnreadCount
-  );
-
-  const router = useRouter();
-
-  const handleNewMessage = useCallback(
-    async (message: Message) => {
-      console.log("New message received:", message); // Debugging statement
-      if (message.senderId !== currentMember.id) {
-        await updateReadStatus(message.id);
-      }
-      addMessage(message);
-      // getUnreadMessageCount().then((count) => {
-      //   updateUnreadCount(currentChat.id, count);
-      // });
-    },
-    [addMessage, updateReadStatus, currentMember.id]
-  );
-
-  // const handleReadMessages = useCallback(
-  //   (messageIds: string[]) => {
-  //     setThreadMessages((prevMessages) =>
-  //       prevMessages.map((message) => {
-  //         if (messageIds.includes(message.id)) {
-  //           return { ...message, dateRead: formatShortDateTime(new Date()) };
-  //         } else {
-  //           return message;
-  //         }
-  //       })
-  //     );
-  //     getUnreadMessageCount().then((count) => {
-  //       updateUnreadCount(count);
-  //     });
-  //   },
-  //   [updateUnreadCount]
-  // );
+  const setCurrentChat = useCurrentChatStore((state) => state.setCurrentChat);
+  const resetCurrentChat = useCurrentChatStore((state) => state.resetCurrentChat);
+   const setAllUnreadMessageCount = useRecentChatsStore(
+      (state) => state.setAllUnreadMessageCount
+    );
 
   useEffect(() => {
-    if (initialChat) setCurrentChat(initialChat);
-  }, [initialChat, setCurrentChat]);
-
-  useEffect(() => {
-    // getUnreadMessageCount().then((count) => {
-    //   updateUnreadCount(count);
-    // });
-    if (currentChat && currentChat.id && !channelRef.current && initialChat) {
-      console.log("Subscribing to channel:", initialChat.id); // Debugging statement
-      channelRef.current = pusherClient.subscribe(`private-${initialChat.id}`);
-      channelRef.current.bind("new-message", (message: Message) => {
-        console.log("Binding 'new-message' to private channel:", message); // Debugging statement
-        handleNewMessage(message);
-      });
-      channelRef.current.bind("message-read", (data: { messageId: string }) => {
-        console.log("Binding 'message-read' to private channel:", data.messageId); // Debugging statement
-        updateMessageReadStatus(data.messageId);
-      });
-      // channelRef.current.bind("read-messages", handleReadMessages);
+    if (initialChat) {
+      setCurrentChat(initialChat);
     }
 
     return () => {
-      if (channelRef.current && channelRef.current.subscribed) {
-        channelRef.current.unsubscribe();
-        channelRef.current.unbind("new-message", handleNewMessage);
-        channelRef.current.unbind("message-read", updateMessageReadStatus);
-        // channelRef.current.unbind("read-messages", handleReadMessages);
-      }
+      resetCurrentChat();
     };
-  }, [currentChat?.id, handleNewMessage]);
+  }, [initialChat, setCurrentChat]);
+
+  useEffect(() => {
+    setAllUnreadMessageCount();
+  }, [currentChat?.id, setAllUnreadMessageCount]);
 
   let chatThread: React.JSX.Element;
   if (
@@ -135,7 +66,11 @@ export default function ChatThread({
       </>
     );
   } else {
-    chatThread = <p>No messages yet</p>;
+    chatThread = (
+      <li key={0} className="my-4 pl-1">
+        Start chatting with {chatPartner.name}.
+      </li>
+    );
   }
 
   return <ul className="flex flex-col">{chatThread}</ul>;
