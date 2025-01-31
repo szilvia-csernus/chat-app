2557fc2bdd133eaa5d752ccc3ea52f3652e272df
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-31 18:05:14 +0000

    update ChatPageLayout

--- src/app/chats/[chatId]/layout.tsx
+++ src/app/chats/[chatId]/layout.tsx
@@ -1,33 +1,21 @@
-import { ReactNode } from "react";
-import { notFound, redirect } from "next/navigation";
-import { Card } from "@heroui/card";
-import { auth } from "@/auth";
+import React from "react";
 import Sidebar from "./(sidebar)/Sidebar";
-import { getCurrentProfileId } from "@/app/actions/profileActions";
+import { getCurrentMember } from "@/app/actions/memberActions";
+import { redirect } from "next/navigation";
 
-export default async function ChatLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  const session = await auth();
 
-  if (!session || !session.user || !session.user.id) {
-    return redirect("/login");
-  }
+export default async function ChatPageLayout({children}: {children: React.ReactNode}) {
 
-  const currentProfileId = await getCurrentProfileId();
-  if (!currentProfileId) return redirect("/profile/complete-profile");
+  const currentMember = await getCurrentMember();
+  if (!currentMember) return redirect("/profile/complete-profile");
 
   return (
     <div className="grid grid-cols-12 gap-1">
       <div className="col-span-5 lg:col-span-4">
-        <Sidebar currentProfileId={currentProfileId} />
+        <Sidebar currentMemberId={currentMember.id} />
       </div>
       <div className="col-span-7 lg:col-span-8">
-        <Card className="w-full h-[85vh] p-5 overflow-auto py-3 border-1 border-gray-300 bg-background">
-          {children}
-        </Card>
+        {children}
       </div>
     </div>
   );
