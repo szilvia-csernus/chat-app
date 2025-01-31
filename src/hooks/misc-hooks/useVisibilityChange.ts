80d53f3f23bf2d8548adef56d7ee08c6a0bb830a
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-31 18:11:13 +0000

    add useVisibilityChange hook

---
+++ src/hooks/misc-hooks/useVisibilityChange.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export function useVisibilityChange(): boolean {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleVisibilityChange = () => {
+      setIsVisible(document.visibilityState === "visible");
+    };
+
+    // Set initial state
+    handleVisibilityChange();
+
+    // Add event listener
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    // Cleanup
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, []);
+
+  return isVisible;
+}
