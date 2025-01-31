c1632fc59cb98e2dc81adfe522b0a9f8f0fa8a3c
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-01-31 18:04:26 +0000

    add messageActions

---
+++ src/app/actions/messageActions.ts
@@ -0,0 +1,139 @@
+"use server";
+
+import { messageSchema, MessageSchema } from "@/lib/schemas/messageSchema";
+import { ActionResult } from "@/types";
+import { getCurrentProfileId } from "./profileActions";
+import { prisma } from "@/prisma";
+import { Message } from "@prisma/client";
+
+import { pusherServer } from "@/lib/pusher";
+import { redirect } from "next/navigation";
+import { authWithError } from "./authActions";
+
+
+/** Creates a message in the given chat. */
+export async function createMessage(
+  chatId: string,
+  chatPartnerId: string,
+  data: MessageSchema
+): Promise<ActionResult<Message>> {
+  await authWithError();
+
+  try {
+    const profileId = await getCurrentProfileId();
+    if (!profileId) {
+      return { status: "error", error: "Profile not found" };
+    }
+    const validated = messageSchema.safeParse(data);
+
+    if (!validated.success) {
+      return { status: "error", error: validated.error.errors };
+    }
+
+    const { content } = validated.data;
+
+    
+    const chat = await prisma.conversation.findUnique({
+      where: {
+        id: chatId,
+      },
+    });
+
+    if (!chat) {
+      return { status: "error", error: "Chat not found" };
+    }
+
+    if (chat.profile1Id !== profileId && chat.profile2Id !== profileId) {
+      return { status: "error", error: "You are not authorized to send messages to this chat" };
+    }
+
+    if (chat.profile1Id !== chatPartnerId && chat.profile2Id !== chatPartnerId) {
+      return { status: "error", error: "Chat partner not found" };
+    }
+
+    const message = await prisma.message.create({
+      data: {
+        content,
+        senderId: profileId,
+        recipientId: chatPartnerId,
+        conversationId: chatId,
+      }
+    });
+
+    await pusherServer.trigger(
+      `private-chat-${chatId}`,
+      "new-message",
+      { chatId: chatId, message }
+    );
+
+    console.log("created message: ", message);
+    return { status: "success", data: message };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+/** Updates all messages with a read:true status in a given
+ * chat for a given recipient. */
+export const updateMessagesWithReadStatus = async (
+  chatId: string, recipientId: string
+) => {
+  await authWithError();
+
+  // Find the message IDs first so we don't have to fetch them twice
+  const messages = await prisma.message.findMany({
+    where: { conversationId: chatId, recipientId, read: false },
+    select: { id: true },
+  });
+
+  const messageIds = messages.map((message) => message.id);
+
+  // Update the messages to set read status
+  await prisma.message.updateMany({
+    where: { id: { in: messageIds } },
+    data: { read: true },
+  });
+
+  // Trigger Pusher events for each message
+  for (const messageId of messageIds) {
+    await pusherServer.trigger(`private-chat-${chatId}`, "message-read", {messageId});
+  }
+};
+
+/** Updates a message with a read:true status. */
+export async function updateReadStatus(messageId: string) {
+  await authWithError();
+
+  try {
+    const currentProfileId = await getCurrentProfileId();
+    const message = await prisma.message.findUnique({
+      where: {
+        id: messageId,
+      },
+    });
+
+    if (!currentProfileId || !message || message.recipientId !== currentProfileId) {
+      return null;
+    }
+
+    await prisma.message.update({
+      where: {
+        id: messageId,
+      },
+      data: {
+        read: true,
+      },
+    });
+
+    await pusherServer.trigger(
+      `private-chat-${message.conversationId}`,
+      "message-read",
+      {messageId}
+    );
+  } catch (error) {
+    console.error(error);
+    return null
+  }
+}
+
