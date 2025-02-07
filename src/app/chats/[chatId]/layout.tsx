64e3a35479211fe572c542e4a2ca63467341afe9
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-02-07 19:09:03 +0000

    edit styles

--- src/app/chats/[chatId]/layout.tsx
+++ src/app/chats/[chatId]/layout.tsx
@@ -10,13 +10,11 @@ export default async function ChatPageLayout({children}: {children: React.ReactN
   if (!currentMember) return redirect("/profile/complete-profile");
 
   return (
-    <div className="grid grid-cols-12 gap-1">
+    <div className="grid grid-cols-12 gap-1 h-[85vh]">
       <div className="col-span-5 lg:col-span-4">
         <Sidebar currentMemberId={currentMember.id} />
       </div>
-      <div className="col-span-7 lg:col-span-8">
-        {children}
-      </div>
+      <div className="col-span-7 lg:col-span-8">{children}</div>
     </div>
   );
 }
