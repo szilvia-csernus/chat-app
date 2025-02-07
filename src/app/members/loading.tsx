e8f381ba94139368e6a1bfcdb192d4b699555f53
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-02-07 19:16:06 +0000

    add loading page for members

--- src/app/members/loading.tsx
+++ src/app/members/loading.tsx
@@ -1,10 +1,15 @@
-import { Spinner } from "@heroui/react";
-import React from "react";
+"use client";
 
-export default function Loading() {
+import { Card, Skeleton } from '@heroui/react';
+
+export default function Loader() {
   return (
-    <div className="flex justify-center items-center vertical-center">
-      <Spinner label="Loading..." color="secondary" labelColor="secondary" />
+    <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-8">
+      {Array.from([1, 2, 3]).map((el) => (
+        <Card key={el} fullWidth>
+          <Skeleton className="aspect-square object-cover transition-transform w-full h-full bg-primary" />
+        </Card>
+      ))}
     </div>
   );
 }
