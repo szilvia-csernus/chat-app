f675609913f064c4f2c389f6fee8a654fc38b952
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-02-07 19:08:25 +0000

    edit styles

--- src/app/chats/[chatId]/(sidebar)/RecentChat.tsx
+++ src/app/chats/[chatId]/(sidebar)/RecentChat.tsx
@@ -38,13 +38,13 @@ export default function RecentChat({
           />
           {online && (
             <div className="absolute top-9 left-11 z-20">
-              <PresenceDot outlineColor="white" />
+              <PresenceDot />
             </div>
           )}
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col text-gray-800 dark:text-white">
           <div>{chatPartner.name}</div>
-          <div className="text-xs text-gray-500 min-w-full relative">
+          <div className="text-xs text-gray-400 min-w-full relative">
             {message || "No messages yet"}
             {unreadMessageCount > 0 && (
               <div className="absolute bottom-2 left-[125px] bg-secondary rounded-full text-white text-xs w-5 h-5 flex items-center justify-center">
