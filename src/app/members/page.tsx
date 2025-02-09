58d67d8e855b9ba4c7ebb901a0b424d352ce9224
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-02-09 16:05:02 +0000

    refactor map functions

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -6,24 +6,15 @@ import { notFound } from 'next/navigation';
 
 export const dynamic = "force-dynamic";
 
-
 export default async function MembersPage() {
   await authWithRedirect();
 
-  const membersData = await getMembers();
-  const members = membersData ? membersData.map(member => ({
-    id: member.id,
-    name: member.user.name || '',
-    image: member.user.image || '',
-  })) : null;
+  const members = await getMembers();
 
   const currentMember = await getCurrentMember();
-
   if (!currentMember) return notFound();
 
   return (
-    <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-8 text-primary">
-      <MembersList members={members} currentMember={currentMember}/>
-    </div>
-  );
+      <MembersList members={members} currentMember={currentMember} />
+  )
 }
