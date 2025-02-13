0c4d11ffec4e7bdd199ab413588539cf2302c325
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-02-13 23:47:33 +0000

    update MainNav's styles

--- src/components/navbar/MainNav.tsx
+++ src/components/navbar/MainNav.tsx
@@ -26,38 +26,42 @@ type MainNavProps = {
   photoUrl: string;
 };
 
-export default function MainNav({ currentProfileId, userName, photoUrl }: MainNavProps) {
+export default function MainNav({
+  currentProfileId,
+  userName,
+  photoUrl,
+}: MainNavProps) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
-  const allUnreadMessageCount = useAppSelector(selectAllUnreadMessageCount);
 
+  const allUnreadMessageCount = useAppSelector(selectAllUnreadMessageCount);
 
   usePresenceChannel(currentProfileId);
   usePrivateChatChannels(currentProfileId);
-  
 
   const menuItems = [
     { href: "/members", label: "Members" },
     { href: "/chats", label: "Chats", badge: allUnreadMessageCount },
   ];
 
-
   return (
     <Navbar
       isMenuOpen={isMenuOpen}
       onMenuOpenChange={setIsMenuOpen}
       maxWidth="2xl"
-      className="bg-gradient-to-r from-slate-700 to-teal-700 p-2 w-full max-w-3xl mx-auto"
+      className="bg-gradient-to-r from-slate-700 to-teal-700 p-2 w-full max-w-4xl mx-auto"
       classNames={{
         item: ["text-m", "text-white", "data-[active=true]:text-orange-300"],
       }}
     >
       <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="text-white"
-        />
+        {currentProfileId && (
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="text-white"
+          />
+        )}
       </NavbarContent>
+      {/* Logo */}
       <NavbarBrand as={Link} href="/">
         <HiOutlineChatBubbleLeftRight size={40} className="text-teal-200" />
         <div className="font-bold text-xl  p-1">
@@ -65,27 +69,39 @@ export default function MainNav({ currentProfileId, userName, photoUrl }: MainNa
           <span className="text-accent">APP</span>
         </div>
       </NavbarBrand>
-      <NavbarMenu className="top-20 z-40 h-auto bg-gradient-to-r from-slate-700 to-teal-700 text-white">
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={index}>
-            <Link href={item.href} onClick={() => setIsMenuOpen(false)}>
-              {item.label}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-      <NavbarContent className="hidden sm:flex gap-6" justify="center">
-        {menuItems.map((item, index) => (
-          <NavLink
-            key={index}
-            href={item.href}
-            label={item.label}
-            badge={item.badge ? item.badge : null}
-            onClick={() => setIsMenuOpen(false)}
-          />
-        ))}
-      </NavbarContent>
+      {/* Mobile Menu */}
+      {currentProfileId && (
+        <NavbarMenu className="top-20 z-40 h-auto bg-gradient-to-r from-slate-700 to-teal-700 text-white">
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem
+              key={index}
+              className="mx-auto my-3 uppercase text-center"
+            >
+              <div className="mb-6 text-center">*</div>
+              <Link href={item.href} onClick={() => setIsMenuOpen(false)}>
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+          <div className="my-4 text-center">*</div>
+        </NavbarMenu>
+      )}
+      {/* Desktop Menu */}
+      {currentProfileId && (
+        <NavbarContent className="hidden sm:flex gap-6" justify="center">
+          {menuItems.map((item, index) => (
+            <NavLink
+              key={index}
+              href={item.href}
+              label={item.label}
+              badge={item.badge ? item.badge : null}
+              onClick={() => setIsMenuOpen(false)}
+            />
+          ))}
+        </NavbarContent>
+      )}
       <NavbarContent justify="end">
+        {/* User Menu */}
         <>
           {userName ? (
             <UserMenu userName={userName} photoUrl={photoUrl} />
@@ -95,6 +111,7 @@ export default function MainNav({ currentProfileId, userName, photoUrl }: MainNa
                 as={Link}
                 href="/login"
                 isIconOnly
+                radius="full"
                 variant="light"
                 className="text-white"
               >
