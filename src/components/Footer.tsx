2021ecdd771faf7d587411bf581787f87dc82dcf
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-02-14 11:51:19 +0000

    add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+export default function Footer() {
+
+
+  return (
+    <div className="block h-[80px] bg-gradient-to-r from-slate-700 to-teal-700 p-2 w-full max-w-4xl mx-auto flex justify-center items-center text-white">
+      <p>
+        Got questions? Contact me <span className="text-accent underline cursor-pointer">here</span>
+      </p>
+    </div>
+  );
+}
