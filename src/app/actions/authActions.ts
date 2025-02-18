02d2718160c2b1d4c1508b0cc7835d55d83775d6
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-02-18 22:53:23 +0000

    add deleteUser function

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -3,7 +3,7 @@
 import { auth, signOut } from "@/auth";
 import { prisma } from "@/prisma";
 import { redirect } from "next/navigation";
-
+import { deleteImageFromCloudinary } from "./photoActions";
 
 /** Authentication function that returns null if
  * user is authenticated, otherwise redirects to login */
@@ -16,7 +16,7 @@ export async function authWithRedirect() {
     return null;
   } catch (error) {
     redirect("/login");
-}
+  }
 }
 
 /** Authentication function that returns null if
@@ -38,11 +38,11 @@ export async function getCurrentUserId() {
   try {
     const session = await auth();
     if (!session?.user?.id) {
-      return null
+      return null;
     }
     return session?.user?.id;
   } catch (error) {
-    return null
+    return null;
   }
 }
 
@@ -51,19 +51,94 @@ export async function getCurrentUser() {
   try {
     const session = await auth();
     if (!session?.user?.id) {
-      return null
+      return null;
     }
     return prisma.user.findUnique({
       where: {
         id: session?.user?.id,
       },
-    })
+    });
   } catch (error) {
-    return null
+    return null;
   }
 }
 
 /** Signs out user */
 export async function signOutUser() {
   await signOut({ redirectTo: "/" });
+}
+
+/** Delete User */
+export async function deleteUser(userId: string) {
+  try {
+    // Find the user's profile
+    const profile = await prisma.profile.findUnique({
+      where: { userId },
+    });
+
+    if (!profile) {
+      throw new Error("Profile not found");
+    }
+
+    console.log("Profile found: ", profile);
+
+    const profileId = profile.id;
+
+    // Mark conversations inactive
+    await prisma.conversation.updateMany({
+      where: { profiles: { some: { id: profileId } } },
+      data: { inactive: true },
+    });
+
+    console.log("Conversations marked as inactive");
+
+    // Mark messages as deleted
+    await prisma.message.updateMany({
+      where: { senderId: profileId },
+      data: { content: "Deleted message", deleted: true },
+    });
+
+    console.log("Messages marked as deleted");
+
+    // Find the user's photo
+    const userPhoto = await prisma.photo.findFirst({
+      where: { userId },
+    });
+
+    console.log("User photo found: ", userPhoto);
+
+    if (userPhoto) {
+      // Delete the photo from Cloudinary
+      await deleteImageFromCloudinary(userPhoto.cloudinaryImageId);
+    }
+
+    console.log("User photo deleted");
+
+    await signOutUser();
+
+    console.log("User signed out");
+
+    // Delete the user account
+    await prisma.user.delete({
+      where: { id: userId },
+    });
+
+    console.log("User deleted");
+
+    // Delete profile records and mark profile as deleted
+    await prisma.profile.update({
+      where: { id: profileId },
+      data: {
+        country: "",
+        gender: "",
+        deleted: true,
+      },
+    });
+
+    console.log("Profile deleted");
+    
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error deleting user");
+  }
 }
