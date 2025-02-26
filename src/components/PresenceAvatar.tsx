3a05f6651c468c6e8cd6a66b0ce3e9bbff095127
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-02-26 23:10:29 +0000

    refactor PresenceAvatar component to handle deleted state and improve online status visibility

--- src/components/PresenceAvatar.tsx
+++ src/components/PresenceAvatar.tsx
@@ -4,13 +4,30 @@ import React from 'react'
 type Props = {
   src?: string;
   online: boolean;
+  deleted: boolean;
   className?: string;
+  own?: boolean;
 }
 
-export default function PresenceAvatar({ src, online, className="" }: Props) {
+export default function PresenceAvatar({ src, online, deleted, className="", own }: Props) {
   return (
-    <Badge content="" color="success" shape='circle' placement='bottom-right' isInvisible={!online}>
-      <Avatar src={src || '/images/user.png'} alt="User avatar" className={className}/>
-    </Badge>
-  )
+    <div className="relative flex">
+      <Badge
+        content=""
+        color="success"
+        shape="circle"
+        placement="bottom-right"
+        isInvisible={!online || deleted || own}
+      >
+        <Avatar
+          src={src || "/images/user.png"}
+          alt="User avatar"
+          className={className}
+        />
+      </Badge>
+      {deleted && (
+        <div className="absolute bottom-[-9px] bg-gray-400 dark:bg-gray-600 px-1 text-[10px] border-1 rounded-full ">Deleted</div>
+      )}
+    </div>
+  );
 }
