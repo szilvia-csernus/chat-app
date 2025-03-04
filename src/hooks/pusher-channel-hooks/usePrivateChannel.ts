99658ff40a02f9502f9561f6b5df9eb0724f3cb5
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-04 16:38:08 +0000

    feat: add usePrivateChannel hook for managing private chat channels and member updates

---
+++ src/hooks/pusher-channel-hooks/usePrivateChannel.ts
@@ -0,0 +1,154 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { Channel } from "pusher-js";
+import { pusherClient } from "@/lib/pusher";
+import { useCallback } from "react";
+// import {
+//   addRecentChat,
+//   setAllUnreadMessageCount,
+//   deactivateRecentChat,
+// } from "@/redux-store/features/recentChatsSlice";
+// import { useAppDispatch, useAppSelector } from "@/redux-store/hooks";
+// import {
+//   deactivateCurrentChat,
+// } from "@/redux-store/features/currentChatSlice";
+import { ChatData, CurrentMember, Member } from "@/types";
+// import {
+//   addChatPartner,
+//   removeChatPartner,
+// } from "@/redux-store/features/chatPartnersSlice";
+import {
+  addMember,
+  updateChatting,
+  updateMemberWithDeletedStatus,
+} from "@/redux-store/features/membersSlice";
+import { getMembers } from "@/app/actions/memberActions";
+import { AppStore } from "@/redux-store/store";
+import { addNewChat, deactivateChat } from "@/redux-store/features/chatsSlice";
+import { updateMessagesWithReadStatus } from "@/app/actions/messageActions";
+import { updateMessagesWithDeletedStatus } from "@/redux-store/features/messagesSlice";
+
+type Props = {
+  store: AppStore;
+  currentMember: CurrentMember | null;
+};
+
+export const usePrivateChannel = ({ store, currentMember }: Props) => {
+  console.log("usePrivateChannel is running");
+  
+  // Ref is used to prevent the creation of multiple channels when the component re-renders
+  const privateChannelRef = useRef<Channel | null>(null);
+
+  const currentMemberId = currentMember?.id;
+
+  const handleNewChat = useCallback(
+    (data: { newChat: ChatData; chatPartnerId: string }) => {
+      console.log("usePrivateChannel: New chat received", data.newChat.id);
+      store.dispatch(addNewChat(data.newChat));
+      // add new chat partner
+      store.dispatch(
+        updateChatting({
+          memberId: data.chatPartnerId,
+          chatId: data.newChat.id,
+        })
+      );
+    },
+    [addNewChat, updateChatting, store]
+  );
+
+  const handleChatInactive = useCallback(
+    (chatId: string, messageIds: string[] ) => {
+      console.log("usePrivateChannel: handleChatInactive", chatId);
+      store.dispatch(deactivateChat(chatId));
+      store.dispatch(updateMessagesWithDeletedStatus(messageIds));
+    },
+    [deactivateChat, store]
+  );
+
+  const handleNewMember = useCallback(
+    async (newMember: Member) => {
+      // add newly-signed-up member to redux store on other people's devices
+      console.log("usePrivateChannel: New member data", newMember);
+      if (newMember) {
+        console.log(
+          "usePrivateChannel: Dispatching addMember for new member",
+          newMember
+        );
+        store.dispatch(addMember(newMember));
+      }
+    },
+    [addMember, store]
+  );
+
+  // when a member is deleted from the database
+  const handleDeleteMember = useCallback(
+    async (memberId: string) => {
+      // remove member from redux store on other people's devices
+      console.log("usePrivateChannel: Deleting member", memberId);
+      const currentMembers = await getMembers();
+      const allMemberIds = currentMembers?.map((m) => m.id);
+      console.log("usePrivateChannel: All member ids", allMemberIds);
+      console.log("usePrivateChannel: dispatching removeMember: ", memberId);
+      store.dispatch(updateMemberWithDeletedStatus(memberId));
+    },
+    [store]
+  );
+
+  useEffect(() => {
+    if (!currentMemberId) return;
+
+    console.log(
+      "usePrivateChannel: Subscribing to private channel",
+      `private-${currentMemberId}`
+    );
+    const privateChannel = (privateChannelRef.current = pusherClient.subscribe(
+      `private-${currentMemberId}`
+    ));
+    privateChannel.bind(
+      "new-chat",
+      (data: { newChat: ChatData; chatPartnerId: string }) => {
+        handleNewChat(data);
+      }
+    );
+    privateChannel.bind(
+      "chat-inactive",
+      (data: {chatId: string, messageIds: string[]}) => {
+        console.log(
+          "usePrivateChannel: Chat inactive event received",
+          data.chatId
+        );
+        handleChatInactive(data.chatId, data.messageIds);
+      }
+    );
+    privateChannel.bind("new-member", (data: { newMember: Member }) => {
+      console.log(
+        "usePrivateChannel: New member event received",
+        data.newMember
+      );
+      handleNewMember(data.newMember);
+    });
+    privateChannel.bind("delete-member", (data: { memberId: string }) => {
+      console.log(
+        "usePrivateChannel: Member deleted event received",
+        data.memberId
+      );
+      handleDeleteMember(data.memberId);
+    });
+
+    // Cleanup function to unsubscribe from channels
+    return () => {
+      const privateChannel = privateChannelRef.current;
+      if (privateChannel) {
+        console.log(
+          "usePrivateChannel: Unsubscribing from private channel",
+          `private-${currentMemberId}`
+        );
+        privateChannel.unbind();
+        pusherClient.unsubscribe(`private-${currentMemberId}`);
+      }
+    };
+  }, [currentMemberId]);
+
+  return privateChannelRef.current;
+};
