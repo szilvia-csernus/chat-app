1e6398adfc89fd4083745b036e541ed13b136324
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-04 16:35:12 +0000

    refactor MembersList component to enhance member retrieval logic, utilize existing member IDs, and improve rendering of member cards

--- src/app/members/MembersList.tsx
+++ src/app/members/MembersList.tsx
@@ -1,29 +1,50 @@
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/redux-store/hooks";
 import MemberCard from "./MemberCard";
-import { Member } from "@/types";
+import { CurrentMember, Member } from "@/types";
+import { selectExistingMemberIds } from "@/redux-store/features/membersSlice";
+import { setCurrentMember } from "@/redux-store/features/currentMemberSlice";
+import { useEffect } from "react";
+
 
 type MembersListProps = {
-  members: Member[] | null;
-  currentMember: Member;
+  currentMember: CurrentMember;
 };
 
 export default function MembersList({
-  members,
   currentMember,
+
 }: MembersListProps) {
-  console.log("Executing MemberList on the server");
+  const dispatch = useAppDispatch();
+  
+  useEffect(() => {
+    dispatch(setCurrentMember(currentMember));
+  }, [currentMember]);
+
+
+  const existingMemberIds = useAppSelector(selectExistingMemberIds);
 
   return (
-    <div className="flex-grow h-full overflow-scroll scrollbar-hide scroll-smooth p-5 border-1 border-slate-300 dark:border-slate-500 bg-zig-zag grid grid-cols-2 min-[500px]:grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 sm:gap-8 text-primary">
-      {members &&
-        members.map((member) => {
-          return (
-            <MemberCard
-              key={member.id}
-              member={member}
-              currentMember={currentMember}
-            />
-          );
-        })}
+    <div className="flex-grow h-full overflow-scroll scrollbar-hide scroll-smooth p-5 border-1 border-slate-300 dark:border-slate-800 bg-zig-zag text-slate-600 dark:text-slate-300">
+      <div className="grid grid-cols-2 min-[500px]:grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 sm:gap-8 ">
+        {existingMemberIds.length > 0 &&
+          existingMemberIds.map((id) => {
+            return (
+              <MemberCard
+                key={id}
+                memberId={id}
+              />
+            );
+          })}
+      </div>
+      {existingMemberIds.length === 0 && (
+        <div>
+          <h1 className="text-xl font-semibold mt-5 justify-self-center">
+            No members to show.
+          </h1>
+        </div>
+      )}
     </div>
   );
 }
