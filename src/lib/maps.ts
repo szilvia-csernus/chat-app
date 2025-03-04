45294ef12588860b3611cbb22d5ce34aaf69a17e
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-04 16:38:26 +0000

    refactor: enhance mapping functions for chat and member data, improving type handling and serialization

--- src/lib/maps.ts
+++ src/lib/maps.ts
@@ -1,61 +1,137 @@
-import { ChatPartner, CPData, RCData, RecentChat, Chat, CData, ChatProfile, Member } from "@/types";
-import { serializeDate, serializeChatProfile } from "@/lib/serialize";
+import {
+  ChatData,
+  ProfileData,
+  Member,
+  Members,
+  SerializedMessage,
+  RawChatData,
+  CurrentMember,
+  CurrentProfileData,
+  SerializedMessages,
+} from "@/types";
+import {
+  serializeDate,
+  serializeCurrentProfileDataToCurrentMember,
+  serializeProfileDataToMember,
+} from "@/lib/serialize";
 
-
-function mapChatProfileToMemberType(profile: ChatProfile): Member {
-  return serializeChatProfile(profile)
+function mapProfileDataToMember(profile: ProfileData): Member {
+  return serializeProfileDataToMember(profile);
 }
 
-function mapCPDataToChatPartnerType(currentProfileId: string, chatData: CPData): ChatPartner | null {
-
-  const profile = chatData.profiles.filter(p => p.id !== currentProfileId)[0];
-  if (!profile) return null;
-  return {
-    chatPartner: mapChatProfileToMemberType(profile),
-    chatId: chatData.id,
-  }
+export function mapProfilesDataToMembers(profiles: ProfileData[]): Member[] {
+  return profiles.map((profile) => mapProfileDataToMember(profile));
 }
 
-export function mapCPDataListToChatPartnerList(
-  currentProfileId: string | null,
-  chatPartnersData: CPData[] | null
-): ChatPartner[] {
-  if (!currentProfileId || !chatPartnersData) return [];
-
-  const chatPartnersList = chatPartnersData.map((cp) =>
-    mapCPDataToChatPartnerType(currentProfileId, cp)
-  ); 
-
-  return chatPartnersList.filter(cp => cp !== null);
-  
+export function mapProfileDataToCurrentMember(
+  profile: CurrentProfileData
+): CurrentMember {
+  return serializeCurrentProfileDataToCurrentMember(profile);
 }
 
-function mapRCDataToRecentChatsType(rcData: RCData): RecentChat {
-  return {
-    id: rcData.id,
-    participants: rcData.profiles.map(mapChatProfileToMemberType),
-    lastMessage: rcData.messages[0]?.content || "",
-    unreadMessageCount: rcData._count.messages,
-  }
+// function mapCPDataToChatPartnerType(
+//   currentProfileId: string,
+//   chatData: CPData
+// ): ChatPartner | null {
+//   const profile = chatData.profiles.filter((p) => p.id !== currentProfileId)[0];
+//   if (!profile) return null;
+//   return {
+//     chatPartner: mapProfileDataToMemberType(profile),
+//     chatId: chatData.id,
+//   };
+// }
+
+// export function mapCPDataListToChatPartnerList(
+//   currentProfileId: string | null,
+//   chatPartnersData: CPData[] | null
+// ): ChatPartner[] {
+//   if (!currentProfileId || !chatPartnersData) return [];
+
+//   const chatPartnersList = chatPartnersData.map((cp) =>
+//     mapCPDataToChatPartnerType(currentProfileId, cp)
+//   );
+
+//   return chatPartnersList.filter((cp) => cp !== null);
+// }
+
+// function mapRCDataToRecentChatsType(rcData: RCData): RecentChat {
+//   return {
+//     id: rcData.id,
+//     chatPartnerId: rcData.profiles[0].id,
+//     lastMessage: rcData.messages[0]?.content || "",
+//     unreadMessageCount: rcData._count.messages,
+//     inactive: rcData.inactive,
+//   };
+// }
+
+// function mapRCDataToChatData(rcData: RCData): ChatData {
+//   let messageList: { [key: string]: SerializedMessage } = {};
+//   let messageIds: string[] = [];
+//   rcData.messages.forEach((message) => {
+//     (messageList[message.id] = {
+//       ...message,
+//       createdAt: serializeDate(message.createdAt),
+//     }),
+//       messageIds.push(message.id);
+//   });
+//   return {
+//     id: rcData.id,
+//     chatPartnerId: rcData.profiles[0].id,
+//     messages: messageList,
+//     messageIds,
+//     inactive: rcData.inactive,
+//     unreadMessageCount: rcData._count.messages,
+//   }
+// };
+
+// export function mapRCDataListToRecentChatsList(
+//   rcData: RCData[] | null
+// ): RecentChat[] {
+//   if (!rcData) return [];
+
+//   return rcData.map((chat) => mapRCDataToRecentChatsType(chat));
+// }
+
+export function mapRawChatDataListToChatsAndMessages(
+  rcData: RawChatData[] | null
+): { chats: ChatData[]; messages: SerializedMessage[] } {
+  if (!rcData) return { chats: [], messages: [] };
+
+  const chats: ChatData[] = [];
+  const messages: SerializedMessage[] = [];
+  rcData.forEach(rc => {
+    const result = mapRawChatDataToChatAndMessages(rc);
+    if (result && result.chat && result.messages) {
+      chats.push(result.chat);
+      messages.push(...result.messages);
+    }
+  })
+
+  return { chats, messages };
 }
 
-export function mapRCDataListToRecentChatsList(
-  rcData: RCData[] | null
-): RecentChat[] {
-  if (!rcData) return []
+export function mapRawChatDataToChatAndMessages(
+  rawChatData: RawChatData | null
+): { chat: ChatData; messages: SerializedMessage[] } | null {
+  if (!rawChatData) return null;
 
-  return rcData.map((chat) => mapRCDataToRecentChatsType(chat));
-}
+  let messages: SerializedMessage[] = [];
+  let messageIds: string[] = [];
+  rawChatData.messages.map((message) => {
+    messages.push({
+        ...message,
+        createdAt: serializeDate(message.createdAt),
+    });
+    messageIds.push(message.id);
+  });
 
-export function mapChatDataToChatType(chatData: CData | null): Chat | null {
-  if (!chatData) return null;
-
-  return {
-    id: chatData.id,
-    participants: chatData.profiles.map(mapChatProfileToMemberType),
-    messages: chatData.messages.map((message) => ({
-      ...message,
-      createdAt: serializeDate(message.createdAt),
-    })),
+  const chat = {
+    id: rawChatData.id,
+    chatPartnerId: rawChatData.profiles[0].id,
+    messageIds,
+    inactive: rawChatData.inactive,
+    unreadMessageCount: rawChatData._count.messages,
   };
+
+  return { chat, messages };
 }
