536be8d4fbdd19e4392aec2dd8d9deb387cc9866
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-04 16:32:58 +0000

    enhance loading component with additional skeleton elements for improved user experience

--- src/app/chats/loading.tsx
+++ src/app/chats/loading.tsx
@@ -5,7 +5,16 @@ import { Skeleton } from "@heroui/react";
 export default function Loading() {
   return (
     <div className="grid grid-cols-12 gap-1">
-      <Skeleton className="rounded-xl col-span-5 lg:col-span-4 h-[85vh] dark:bg-gray-700" />
+      <Skeleton className="rounded-xl col-span-5 lg:col-span-4 h-[85vh] dark:bg-gray-700">
+        <div className="flex flex-row gap-2">
+          <Skeleton className="m-1 rounded-full h-[52px] w-[52px]" />
+
+          <div className="my-2 flex flex-col gap-2">
+            <Skeleton className="h-6 w-32 rounded-lg" />
+            <Skeleton className="h-4 w-full rounded-lg" />
+          </div>
+        </div>
+      </Skeleton>
       <Skeleton className="rounded-xl col-span-7 lg:col-span-8 h-[85vh] dark:bg-gray-700" />
     </div>
   );
