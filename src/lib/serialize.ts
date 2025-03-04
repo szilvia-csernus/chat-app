4f4d60b52b2b39920c802911f97249f5095202af
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-04 16:38:38 +0000

    refactor: enhance serialization functions for profile and message data, improving type handling and structure

--- src/lib/serialize.ts
+++ src/lib/serialize.ts
@@ -1,9 +1,8 @@
 // Serialization of data is required by redux
 
-import { ChatProfile, Member, SerializedMessage } from "@/types";
+import { ProfileData, Member, SerializedMessage, CurrentMember, CurrentProfileData } from "@/types";
 import { Message } from "@prisma/client";
 import { formatShortDateTime } from "./utils";
-
 
 export function serializeDate(date: Date) {
   return formatShortDateTime(date); // format is changed from Date to string (in effect, also serialized)
@@ -16,16 +15,31 @@ export function serializeMessage(message: Message): SerializedMessage {
     createdAt: serializeDate(message.createdAt),
     senderId: message.senderId || null,
     read: message.read,
-    deleted: message.deleted
+    deleted: message.deleted,
   };
 }
 
-export function serializeChatProfile(profile: ChatProfile): Member {
+export function serializeProfileDataToMember(profile: ProfileData): Member {
   return {
     id: profile.id,
     name: profile.user?.name || "",
     image: profile.user?.image || null,
     lastActive: serializeDate(profile.lastActive),
-    deleted: profile.deleted
+    deleted: profile.deleted,
+    chatting:
+      profile.conversations.length > 0 ? profile.conversations[0].id : null,
+    online: false,
+  };
+}
+
+export function serializeCurrentProfileDataToCurrentMember(profile: CurrentProfileData): CurrentMember {
+  return {
+    id: profile.id,
+    name: profile.user?.name || "",
+    image: profile.user?.image || null,
+    lastActive: serializeDate(profile.lastActive),
+    deleted: profile.deleted,
+    online: false,
+    lastActiveConversationId: profile.lastActiveConversationId || null,
   };
 }
