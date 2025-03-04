a7c03512e45341077ff1bd6906bc65918d15addf
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-04 16:34:15 +0000

    refactor CurrentChatPartner component to streamline chat partner retrieval and enhance online status display

--- src/app/chats/[chatId]/(main)/CurrentChatPartner.tsx
+++ src/app/chats/[chatId]/(main)/CurrentChatPartner.tsx
@@ -4,67 +4,69 @@ import React from "react";
 import PresenceDot from "@/components/PresenceDot";
 import MemberImage from "@/app/members/MemberImage";
 import { Member } from "@/types";
-import { selectMembersOnline } from "@/redux-store/features/presenceSlice";
+// import { selectMemberOnlineStatus, selectMembersOnline } from "@/redux-store/features/presenceSlice";
 import { useAppSelector } from "@/redux-store/hooks";
-import { selectCurrentChat } from "@/redux-store/features/currentChatSlice";
-import { selectChatPartnerById } from "@/redux-store/features/chatPartnersSlice";
+// import { selectCurrentChat, selectCurrentChatPartnerId } from "@/redux-store/features/currentChatSlice";
+// import { selectChatPartnerById } from "@/redux-store/features/chatPartnersSlice";
 import { IoIosArrowBack } from "react-icons/io";
+import PresenceAvatar from "@/components/PresenceAvatar";
+import { notFound } from "next/navigation";
+import { selectMemberById, selectMemberOnlineStatus } from "@/redux-store/features/membersSlice";
+import { selectCurrentMember } from "@/redux-store/features/currentMemberSlice";
+import { selectCurrentChat } from "@/redux-store/features/chatsSlice";
 
 type Props = {
-  chatPartnerId: string;
   isSidebarOpen: boolean;
   setIsSidebarOpen: (isOpen: boolean) => void;
 };
 
 export default function CurrentChatPartner({
-  chatPartnerId,
   isSidebarOpen,
-  setIsSidebarOpen
+  setIsSidebarOpen,
 }: Props) {
-  const membersOnline = useAppSelector(selectMembersOnline);
-  const chatPartnerData = useAppSelector(selectChatPartnerById(chatPartnerId));
-  if (!chatPartnerData) {
-    return null;
-  }
-  const chatPartner = chatPartnerData.chatPartner;
-  
-  const online = membersOnline.includes(chatPartnerId);
+  const currentChat = useAppSelector(selectCurrentChat);
+  const chatPartnerId = currentChat && currentChat.chatPartnerId;
+  const chatPartner = chatPartnerId && useAppSelector(state => selectMemberById(state, chatPartnerId));
+  const online = !!(chatPartnerId && useAppSelector(state => selectMemberOnlineStatus(state, chatPartnerId)));
 
   return (
-    <div className="m-auto border-b-1 border-slate-300 dark:border-slate-500  bg-white dark:bg-gray-800 flex items-center">
-      <IoIosArrowBack
-        className="sm:hidden ml-2 text-slate-500  dark:text-slate-400"
-        size={30}
-        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-      />
-      <div className="m-2 rounded-full border-1 border-slate-500 overflow-hidden">
-        <MemberImage
-          memberImage={
-            chatPartner.image ? chatPartner.image : "/images/user.png"
-          }
-          memberName={chatPartner.name ? chatPartner.name : ""}
-          width={35}
-          height={35}
-        />
-      </div>
-      <div className="flex flex-col text-gray-800 dark:text-white justify-center">
-        <div className="text-sm">
-          {chatPartner.name}
-          <>
-            {online && (
-              <div className="text-xs text-teal-500 dark:text-teal-300">
-                ACTIVE NOW
+    <>
+      { chatPartner &&
+        <div className="m-auto border-b-1 border-slate-300 dark:border-slate-500  bg-white dark:bg-gray-800 flex items-center relative">
+          <IoIosArrowBack
+            className="sm:hidden ml-2 text-slate-500  dark:text-slate-400"
+            size={30}
+            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          />
+          <div className="mx-1 my-3 ">
+            <PresenceAvatar
+              src={chatPartner.image || "/images/user.png"}
+              online={online}
+              deleted={chatPartner.deleted || false}
+              className="self-end mx-1"
+              own={false}
+            />
+          </div>
+          <div className="flex flex-col text-gray-800 dark:text-white justify-center">
+            <div className="text-sm">
+              {chatPartner && chatPartner.name && chatPartner.name}
+              {(!chatPartner || chatPartner.deleted) && "Deleted User"}
+
+              {chatPartner && !chatPartner.deleted && online && (
+                <div className="text-xs text-teal-500 dark:text-teal-300">
+                  ACTIVE NOW
+                </div>
+              )}
+            </div>
+
+            {!online && (
+              <div className="text-xs text-gray-400 min-w-full relative">
+                Last seen: {chatPartner && chatPartner.lastActive}
               </div>
             )}
-          </>
-        </div>
-
-        {!online && (
-          <div className="text-xs text-gray-400 min-w-full relative">
-            Last seen: {chatPartner.lastActive}
           </div>
-        )}
-      </div>
-    </div>
+        </div>
+      }
+    </>
   );
 }
