68f1d905dc3127eaf9be2b44c35b950468caf25e
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-04 16:33:39 +0000

    refactor ChatPage to remove unused getChatPartner call and update parameter handling

--- src/app/chats/[chatId]/page.tsx
+++ src/app/chats/[chatId]/page.tsx
@@ -1,35 +1,35 @@
 import { notFound, redirect } from "next/navigation";
-import { getChat, getChatPartner } from "@/app/actions/chatActions";
-import { getCurrentMember } from "@/app/actions/memberActions";
+import { getChat } from "@/app/actions/chatActions";
 import { updateMessagesWithReadStatus } from "@/app/actions/messageActions";
 import { authWithRedirect } from "@/app/actions/authActions";
 import Chats from "./Chats";
+import { getCurrentProfile } from "@/app/actions/profileActions";
 
 export const dynamic = "force-dynamic";
 
 export default async function ChatPage({
   params,
 }: {
-  params: { chatId: string };
+  params: Promise<{ chatId: string }>;
 }) {
   await authWithRedirect();
 
-  const currentMember = await getCurrentMember();
-  if (!currentMember) return redirect("/profile/complete-profile");
+  const searchParams = await params;
+  const chatId = searchParams.chatId;
+  console.log("Chat ID", chatId);
+
+  const currentProfile = await getCurrentProfile();
+  if (!currentProfile) return redirect("/profile/complete-profile");
 
   // Update the messages in the database to be marked as read
-  await updateMessagesWithReadStatus(params.chatId);
+  await updateMessagesWithReadStatus(chatId);
 
-  const initialChat = await getChat(params.chatId);
+  const initialChat = await getChat(chatId);
   if (!initialChat) return notFound();
 
-  const chatPartner = await getChatPartner(initialChat.id);
-  if (!chatPartner) return notFound();
 
   return (
     <Chats
-      chatPartner={chatPartner}
-      currentMember={currentMember}
       initialChat={initialChat}
     />
   );
