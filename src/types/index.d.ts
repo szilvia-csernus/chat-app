39db9ea96c7078d92779cde48c932102605be503
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-04 16:38:56 +0000

    refactor: update type definitions for profiles and messages, enhancing structure and adding new properties

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,78 +1,123 @@
-import { Conversation } from "@prisma/client";
+import { Conversation, Profile } from "@prisma/client";
 import { ZodIssue } from "zod";
 
 type ActionResult<T> =
   | { status: "success"; data: T }
   | { status: "error"; error: string | ZodIssue[] };
 
-type ChatProfile = {
+type CurrentProfileData = {
   id: string;
   user: {
     name: string | null;
     image: string | null;
-  };
+  } | null;
   lastActive: Date;
+  deleted: boolean;
+  lastActiveConversationId?: string | null;
+};
+
+type ProfileData = {
+  id: string;
+  user: {
+    name: string | null;
+    image: string | null;
+  } | null;
+  conversations: { id: string }[];
+  lastActive: Date;
+  deleted: boolean;
 };
 
 type Member = {
   id: string;
   name: string;
-  image: string;
+  image: string | null;
   lastActive: string;
-}
+  deleted: boolean;
+  chatting?: string | null;
+  online: boolean;
+};
+
+type Members = {
+  [key: string]: Member;
+};
+
+type CurrentMember = {
+  id: string;
+  name: string;
+  image: string | null;
+  lastActive: string;
+  deleted: boolean;
+  online: boolean;
+  lastActiveConversationId?: string | null;
+};
+
+type RawChatData = {
+  profiles: {
+    id: string;
+  }[];
+  messages: MessageData[];
+  _count: { messages: number };
+} & Conversation;
+
+type ChatData = {
+  id: string;
+  chatPartnerId: string;
+  messageIds: string[];
+  inactive: boolean;
+  unreadMessageCount: number;
+};
+
+type ChatsData = {
+  [key: string]: ChatData;
+};
+
+type MessageData = {
+  id: string;
+  content: string;
+  createdAt: Date;
+  senderId: string | null;
+  read: boolean;
+  deleted: boolean;
+};
 
 type SerializedMessage = {
   id: string;
   content: string;
   createdAt: string;
-  senderId: string;
+  senderId: string | null;
   read: boolean;
+  deleted: boolean;
 };
 
-type Chat = {
-  id: string;
-  participants: Member[];
-  messages: SerializedMessage[];
-}
-
-type CPData = {
-  profiles: ChatProfile[];
-} & Conversation;
-
-type ChatPartner = {
-  chatPartner: Member;
-  chatId: string;
-}
-
-type RCData = {
-  profiles: ChatProfile[];
-  messages: {
-    content: string;
-    createdAt: Date;
-    senderId: string;
-    read: boolean;
-  }[];
-  _count: { messages: number };
-} & Conversation;
+type SerializedMessages = {
+  [key: string]: SerializedMessage;
+};
 
 
-type RecentChat = {
-  id: string;
-  participants: Member[];
-  lastMessage: string;
-  unreadMessageCount: number;
-}
+// type CPData = {
+//   profiles: ProfileData[];
+// } & Conversation;
 
-type CDataMessage = {
-  id: string;
-  content: string;
-  createdAt: Date;
-  senderId: string;
-  read: boolean;
-}
+// type ChatPartner = {
+//   chatPartner: Member;
+//   chatId: string;
+// };
 
-type CData = {
-  profiles: ChatProfile[];
-  messages: CDataMessage[];
-} & Conversation;
 
+
+// type RecentChat = {
+//   id: string;
+//   chatPartnerId: string;
+//   lastMessage: string;
+//   unreadMessageCount: number;
+//   inactive: boolean;
+// };
+
+
+
+// type CData = {
+//   profiles: {
+//     id: string;
+//   }[];
+//   messages: MessageData[];
+// } & Conversation;
