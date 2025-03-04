381cfdabb84dc8a595103afcc09d0504a8e73ca4
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-04 16:34:43 +0000

    refactor SidebarDesktop component to simplify props, enhance current member retrieval, and improve chat list rendering

--- src/app/chats/[chatId]/(sidebar)/SidebarDesktop.tsx
+++ src/app/chats/[chatId]/(sidebar)/SidebarDesktop.tsx
@@ -4,25 +4,27 @@ import React from "react";
 import { Card, CardBody, CardFooter, Button } from "@heroui/react";
 import { useRouter } from "next/navigation";
 import RecentChatsList from "./RecentChatsList";
+import { useAppSelector } from "@/redux-store/hooks";
+import { selectCurrentMember } from "@/redux-store/features/currentMemberSlice";
 
 
-type Props = {
-  currentMemberId: string;
-};
-
-export default function SidebarDesktop({ currentMemberId }: Props) {
+export default function SidebarDesktop() {
   const router = useRouter();
+
+  const currentMember = useAppSelector(selectCurrentMember);
 
   return (
     <Card
       radius="none"
-      className="w-full items-center h-full border-1 border-slate-300 dark:border-slate-500 bg-zig-zag"
+      className="w-full items-center h-full border-1 border-slate-300 dark:border-slate-800 bg-zig-zag"
     >
-      <CardBody className="flex flex-col h-full items-center p-0">
-        <div className="w-full text-xl font-bold pt-4 pb-2 flex justify-center border-b-1 border-slate-300 dark:border-slate-500 text-slate-600 dark:text-slate-200">
+      <CardBody className="flex flex-col items-center p-0">
+        <div className="sticky w-full text-xl font-bold pt-4 pb-2 flex justify-center border-b-1 border-slate-300 dark:border-slate-500 text-slate-600 dark:text-slate-200">
           <span>Your Chats</span>
         </div>
-        <RecentChatsList currentMemberId={currentMemberId} />
+        {currentMember && (
+          <RecentChatsList />
+        )}
       </CardBody>
       <CardFooter className="flex flex-col justify-end pb-3">
         <Button
