69f524db5a6d73ac721adc280be92cadd26efcf8
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-04 16:34:50 +0000

    refactor SidebarMobile component to simplify props, enhance current member retrieval, and improve rendering logic

--- src/app/chats/[chatId]/(sidebar)/SidebarMobile.tsx
+++ src/app/chats/[chatId]/(sidebar)/SidebarMobile.tsx
@@ -4,35 +4,33 @@ import React from "react";
 import { Card, CardBody } from "@heroui/react";
 import { useRouter } from "next/navigation";
 import RecentChatsList from "./RecentChatsList";
+import { useAppSelector } from "@/redux-store/hooks";
+import { selectCurrentMember } from "@/redux-store/features/currentMemberSlice";
 
 type Props = {
-  currentMemberId: string;
-  isSidebarOpen: boolean;
   setIsSidebarOpen: (isOpen: boolean) => void;
 };
 
 export default function SidebarMobile({
-  currentMemberId,
-  isSidebarOpen,
   setIsSidebarOpen,
 }: Props) {
-  const router = useRouter();
+  const currentMember = useAppSelector(selectCurrentMember);
 
   return (
     <>
       <Card
         radius="none"
-        className="items-center h-full m-0 border-1 border-slate-300 dark:border-slate-500 bg-zig-zag"
+        className="items-center h-screen m-0 border-1 border-slate-300 dark:border-slate-800 bg-zig-zag"
       >
         <CardBody className="flex flex-col h-full items-center p-0">
-          <div className="text-xl w-full font-bold pt-4 pb-2 flex justify-center border-b-1 border-slate-300 dark:border-slate-500 text-slate-600 dark:text-slate-200">
+          <div className="text-xl w-screen font-bold pt-4 pb-3 flex justify-center border-b-1 border-slate-300 dark:border-slate-500 text-slate-600 dark:text-slate-200">
             <span>Your Chats</span>
           </div>
-          <RecentChatsList
-            currentMemberId={currentMemberId}
-            isSidebarOpen={isSidebarOpen}
-            setIsSidebarOpen={setIsSidebarOpen}
-          />
+          {currentMember && (
+            <RecentChatsList
+              setIsSidebarOpen={setIsSidebarOpen}
+            />
+          )}
         </CardBody>
       </Card>
     </>
