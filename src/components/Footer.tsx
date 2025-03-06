470d7d05f41562e69cc42bc33bbcdd154afe3e3a
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-06 09:12:24 +0000

    refactor: simplify class structure in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
 
 
   return (
-    <div className="block h-[80px] bg-gradient-to-r from-slate-700 to-teal-700 p-2 w-full max-w-4xl mx-auto flex justify-center items-center text-white">
+    <div className="flex h-[80px] bg-gradient-to-r from-slate-700 to-teal-700 p-2 w-full max-w-4xl mx-auto justify-center items-center text-white">
       <p>
         Got questions? Contact me <span className="text-accent underline cursor-pointer">here</span>
       </p>
