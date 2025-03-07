9413b93767f9a70d89ab22f1a23ac1fed8d1362c
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-07 18:37:36 +0000

    refactor: enhance MessageBox component with improved state management and rendering logic

--- src/app/chats/[chatId]/(main)/MessageBox.tsx
+++ src/app/chats/[chatId]/(main)/MessageBox.tsx
@@ -4,7 +4,7 @@ import PresenceAvatar from "@/components/PresenceAvatar";
 import { useAppSelector } from "@/redux-store/hooks";
 import { CurrentMember, Member } from "@/types";
 import clsx from "clsx";
-import { useEffect, useRef } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import { selectMemberOnlineStatus } from "@/redux-store/features/membersSlice";
 import { selectMessageById } from "@/redux-store/features/messagesSlice";
 
@@ -12,20 +12,62 @@ type MessageBoxProps = {
   messageId: string;
   currentMember: CurrentMember;
   chatPartner: Member;
+  latestDate: { current: string | null };
+  lastMsgSender: { current: "currentMember" | "chatPartner" | null};
 };
 
 export default function MessageBox({
   messageId,
   currentMember,
   chatPartner,
-
+  latestDate,
+  lastMsgSender
 }: MessageBoxProps) {
-  const message = useAppSelector(state => selectMessageById(state, messageId)
+  const message = useAppSelector((state) =>
+    selectMessageById(state, messageId)
   );
+
+  const [date, setDate] = useState<string | null>(null);
+  const [renderCurrentMemberImg, setRenderCurrentMemberImg] = useState<boolean>(true);
+  const [renderChatPartnerImg, setRenderChatPartnerImg] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (message.date === latestDate.current) {
+      setDate(null);
+    } else {
+      setDate(message.date);
+      latestDate.current = message.date;
+    }
+  }, [message.date, latestDate]);
+
+  useEffect(() => {
+    if (message.senderId === currentMember.id && lastMsgSender.current === "currentMember") {
+      setRenderCurrentMemberImg(false);
+    } else {
+      if (message.senderId === currentMember.id) {
+        setRenderCurrentMemberImg(true);
+        lastMsgSender.current = "currentMember";
+      }
+    }
+  })
+
+  useEffect(() => {
+    if (message.senderId === chatPartner.id && lastMsgSender.current === "chatPartner") {
+      setRenderChatPartnerImg(false);
+    } else {
+      if (message.senderId === chatPartner.id) {
+        setRenderChatPartnerImg(true);
+        lastMsgSender.current = "chatPartner";
+      } 
+    }
+  })
+
   const isCurrentMemberSender = message.senderId === currentMember.id;
   const sender = isCurrentMemberSender ? currentMember : chatPartner;
-  const chatPartnerOnline = useAppSelector(state => selectMemberOnlineStatus(state, chatPartner.id));
-  
+  const chatPartnerOnline = useAppSelector((state) =>
+    selectMemberOnlineStatus(state, chatPartner.id)
+  );
+
   const messageEndRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -45,29 +87,35 @@ export default function MessageBox({
   );
 
   const messageContentClasses = clsx(
-    "flex flex-col px-2 py-1 border-1 border-slate-300 dark:border-slate-500",
+    "flex flex-col min-w-32 px-2 py-1 border-1 border-slate-300 dark:border-slate-500",
     {
-      "rounded-l-2xl rounded-tr-2xl bg-cyan-50 dark:bg-cyan-800 text-slate-800 dark:text-white": isCurrentMemberSender,
-      "rounded-r-2xl rounded-tl-2xl bg-white dark:bg-gray-800":
+      "rounded-l-xl rounded-br-xl bg-cyan-50 dark:bg-cyan-800 text-slate-800 dark:text-white":
+        isCurrentMemberSender,
+      "rounded-r-xl rounded-bl-xl bg-white dark:bg-gray-800":
         !isCurrentMemberSender,
       "bg-gray-200 dark:bg-gray-700": message?.deleted,
     }
   );
 
-  const rendermessageHeader = () => (
+  const rendermessageDetails = () => (
     <div
       className={clsx(
-        "flex items-center w-full mt-1 text-secondary dark:text-teal-300",
-        {
-          "justify-end": isCurrentMemberSender,
-        }
+        "flex items-center w-full text-secondary dark:text-teal-300"
+        // {
+        //   "justify-end": isCurrentMemberSender,
+        // }
       )}
     >
-      <div className="flex">
-        <span className="text-xs font-bold ">
+      <div className="flex justify-end gap-2 w-full">
+        {/* <span className="text-xs font-bold mr-2">
           {isCurrentMemberSender ? "You" : sender.name}
-        </span>
-        <span className="text-xs ml-2 ">{message?.createdAt}</span>
+        </span> */}
+        <div className="text-xs">{message?.time}</div>
+        {message?.read && message.senderId === currentMember.id && (
+          <div className="text-xs text-gray-400 italic w-auto">
+            Seen &#10003;
+          </div>
+        )}
       </div>
     </div>
   );
@@ -75,43 +123,63 @@ export default function MessageBox({
   const renderMessageContent = () => (
     <div className="flex flex-col">
       <div className={messageContentClasses}>
-        {rendermessageHeader()}
         <p
-          className={clsx("text-sm py-3 text-gray-800 dark:text-white whitespace-pre-line", {
-           "italic text-gray-500 dark:text-gray-400" : message?.deleted }
+          className={clsx(
+            "text-sm text-gray-800 dark:text-white whitespace-pre-line",
+            {
+              "italic text-xs text-gray-500 dark:text-gray-500":
+                message?.deleted,
+            }
           )}
         >
           {message?.content}
         </p>
+        {rendermessageDetails()}
       </div>
-      {message?.read && message.senderId === currentMember.id ? (
-        <div className="text-xs text-gray-400 italic">Seen &#10003;</div>
-      ) : (
-        <div></div>
-      )}
+      
     </div>
   );
 
   return (
     <>
-      {message && (
-        <div className="grid grid-rows-1">
-          <div
-            className={clsx(
-              "flex gap-2 mb-3",
-              {
-                "justify-end text-right": isCurrentMemberSender,
-              },
-              { "justyfy-start": !isCurrentMemberSender }
-            )}
-          >
-            {!isCurrentMemberSender && renderAvatar()}
-            {renderMessageContent()}
-            {isCurrentMemberSender && renderAvatar()}
-          </div>
-          <div ref={messageEndRef} />
+      {date && (
+        <div
+          suppressHydrationWarning={true}
+          className="w-32 text-center text-xs font-semibold my-4 mx-auto py-1 border-1 bg-cyan-50 dark:bg-cyan-800 text-slate-800 dark:text-white rounded-full border-slate-300 dark:border-slate-500"
+        >
+          {date}
         </div>
       )}
+      <Suspense fallback={<div>Loading message...</div>}>
+        {message && (
+          <div className="grid grid-rows-1">
+            <div
+              className={clsx(
+                "flex gap-2 mb-1 items-start",
+                {
+                  "justify-end text-right": isCurrentMemberSender,
+                },
+                { "justyfy-start": !isCurrentMemberSender }
+              )}
+            >
+              {!isCurrentMemberSender &&
+                (renderChatPartnerImg ? (
+                  renderAvatar()
+                ) : (
+                  <div className="w-10" />
+                ))}
+              {renderMessageContent()}
+              {isCurrentMemberSender &&
+                (renderCurrentMemberImg ? (
+                  renderAvatar()
+                ) : (
+                  <div className="w-10" />
+                ))}
+            </div>
+            <div ref={messageEndRef} />
+          </div>
+        )}
+      </Suspense>
     </>
   );
 }
