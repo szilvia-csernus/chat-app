92cf1af9c4df5d2d76e9e9df59703661e96e5bb6
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-07 18:42:42 +0000

    feat: replace Avatar component with MemberImage and improve layout in PresenceAvatar

--- src/components/PresenceAvatar.tsx
+++ src/components/PresenceAvatar.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Badge } from "@heroui/react";
-import React from 'react'
+import React from "react";
+import MemberImage from "./MemberImage";
 
 type Props = {
   src?: string;
@@ -7,11 +8,16 @@ type Props = {
   deleted: boolean;
   className?: string;
   own?: boolean;
-}
+};
 
-export default function PresenceAvatar({ src, online, deleted, className="", own }: Props) {
+export default function PresenceAvatar({
+  src,
+  online,
+  deleted,
+  own,
+}: Props) {
   return (
-    <div className="relative flex">
+    <div className="relative flex items-end">
       <Badge
         content=""
         color="success"
@@ -19,14 +25,18 @@ export default function PresenceAvatar({ src, online, deleted, className="", own
         placement="bottom-right"
         isInvisible={!online || deleted || own}
       >
-        <Avatar
-          src={src || "/images/user.png"}
-          alt="User avatar"
-          className={className}
+        <MemberImage
+          memberImage={src || "/images/user.png"}
+          memberName="User avatar"
+          width={40}
+          height={40}
+          className="h-[40px] border-1 border-slate-300 dark:border-slate-500 rounded-full z-5"
         />
       </Badge>
       {deleted && (
-        <div className="absolute bottom-[-9px] bg-gray-400 dark:bg-gray-600 px-1 text-[10px] border-1 rounded-full ">Deleted</div>
+        <div className="absolute bottom-[-9px] bg-gray-400 dark:bg-gray-600 px-1 text-[10px] border-1 rounded-full ">
+          Deleted
+        </div>
       )}
     </div>
   );
