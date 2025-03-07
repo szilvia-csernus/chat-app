3a0147db14d3b540b5c020b3aa31d653db4b4d6a
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-07 18:38:03 +0000

    refactor: add console log for currentMember in SidebarMobile component

--- src/app/chats/[chatId]/(sidebar)/SidebarMobile.tsx
+++ src/app/chats/[chatId]/(sidebar)/SidebarMobile.tsx
@@ -14,6 +14,7 @@ export default function SidebarMobile({
   setIsSidebarOpen,
 }: Props) {
   const currentMember = useAppSelector(selectCurrentMember);
+  console.log("SidebarMobile currentMember", currentMember);
 
   return (
     <>
