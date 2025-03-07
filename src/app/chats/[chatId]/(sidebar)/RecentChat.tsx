f5784d3d9af9bbff9b6e2791b8573aec17a5a036
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-07 18:37:53 +0000

    refactor: improve RecentChat component structure and readability

--- src/app/chats/[chatId]/(sidebar)/RecentChat.tsx
+++ src/app/chats/[chatId]/(sidebar)/RecentChat.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import PresenceDot from "@/components/PresenceDot";
-import MemberImage from "@/app/members/MemberImage";
+import MemberImage from "@/components/MemberImage";
 import RecentChatFrame from "./RecentChatFrame";
 import { useAppSelector } from "@/redux-store/hooks";
 import { IoIosArrowForward } from "react-icons/io";
-import { selectMemberById, selectMemberOnlineStatus } from "@/redux-store/features/membersSlice";
+import {
+  selectMemberById,
+  selectMemberOnlineStatus,
+} from "@/redux-store/features/membersSlice";
 import { selectChatById } from "@/redux-store/features/chatsSlice";
 import { selectMessageById } from "@/redux-store/features/messagesSlice";
 
@@ -19,61 +22,72 @@ export default function RecentChat({
   isSidebarOpen,
   setIsSidebarOpen,
 }: Props) {
-
   const chat = useAppSelector((state) => selectChatById(state, chatId));
   const { chatPartnerId, messageIds = [], unreadMessageCount } = chat;
   const lastMessageId = messageIds[messageIds.length - 1];
-  const lastMessage = useAppSelector((messagesState) => selectMessageById(messagesState, lastMessageId)?.content);
-  const chatPartner = useAppSelector(
-    state => selectMemberById(state, chatPartnerId)
+  const lastMessage = useAppSelector(
+    (messagesState) => selectMessageById(messagesState, lastMessageId)?.content
   );
-  const online = useAppSelector(state => selectMemberOnlineStatus(state, chatPartnerId));
+  const chatPartner = useAppSelector((state) =>
+    selectMemberById(state, chatPartnerId)
+  );
+  const online = useAppSelector((state) =>
+    selectMemberOnlineStatus(state, chatPartnerId)
+  );
   const trimmedLastMessage =
-    (lastMessage && lastMessage.length > 25) ? `${lastMessage.slice(0, 25)}...` : lastMessage;
+    lastMessage && lastMessage.length > 25
+      ? `${lastMessage.slice(0, 25)}...`
+      : lastMessage;
 
   return (
-    <RecentChatFrame
-      chatId={chatId}
-      isSidebarOpen={isSidebarOpen}
-      setIsSidebarOpen={setIsSidebarOpen}
-    >
-      <div className="flex items-center">
-        <div className="m-2 rounded-full border-2 border-slate-500 overflow-hidden">
-          <MemberImage
-            memberImage={
-              chatPartner.image ? chatPartner.image : "/images/user.png"
-            }
-            memberName={chatPartner.name ? chatPartner.name : ""}
-            width={50}
-            height={50}
-          />
-          {online && (
-            <div className="absolute top-9 left-11 z-20">
-              <PresenceDot />
+    <>
+      {chatPartner && (
+        <RecentChatFrame
+          chatId={chatId}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        >
+          <div className="flex items-center">
+            <div className="m-2 rounded-full border-2 border-slate-300 dark:border-slate-500 overflow-hidden">
+              <MemberImage
+                memberImage={
+                  chatPartner.image ? chatPartner.image : "/images/user.png"
+                }
+                memberName={chatPartner.name ? chatPartner.name : ""}
+                width={50}
+                height={50}
+              />
+              {online && (
+                <div className="absolute top-9 left-11 z-20">
+                  <PresenceDot />
+                </div>
+              )}
+              {chatPartner.deleted && (
+                <div className="absolute bottom-[2px] left-3 z-20 bg-gray-400 dark:bg-gray-600 px-1 text-[10px] border-1 rounded-full ">
+                  Deleted
+                </div>
+              )}
             </div>
-          )}
-          {chatPartner.deleted && (
-            <div className="absolute bottom-[2px] left-3 z-20 bg-gray-400 dark:bg-gray-600 px-1 text-[10px] border-1 rounded-full ">
-              Deleted
-            </div>
-          )}
-        </div>
-        <div className="flex flex-col text-gray-800 dark:text-white pr-8">
-          <div>{chatPartner.deleted ? "Deleted User" : chatPartner.name}</div>
-          <div className="text-xs text-gray-400 min-w-full">
-            {trimmedLastMessage || "No message to show"}
-            {unreadMessageCount > 0 && (
-              <div className="absolute bottom-6 right-[30px] sm:right-[10px] bg-secondary rounded-full text-white text-xs w-5 h-5 flex items-center justify-center">
-                {unreadMessageCount}
+            <div className="flex flex-col text-gray-800 dark:text-white pr-8">
+              <div>
+                {chatPartner.deleted ? "Deleted User" : chatPartner.name}
               </div>
-            )}
+              <div className="text-xs text-gray-400 min-w-full">
+                {trimmedLastMessage || "No message to show"}
+                {unreadMessageCount > 0 && (
+                  <div className="absolute bottom-6 right-[30px] sm:right-[10px] bg-secondary rounded-full text-white text-xs w-5 h-5 flex items-center justify-center">
+                    {unreadMessageCount}
+                  </div>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <IoIosArrowForward
-        className="sm:hidden ml-2 justify-self-end text-slate-500 dark:text-slate-400"
-        size={30}
-      />
-    </RecentChatFrame>
+          <IoIosArrowForward
+            className="sm:hidden ml-2 justify-self-end text-slate-500 dark:text-slate-400"
+            size={30}
+          />
+        </RecentChatFrame>
+      )}
+    </>
   );
 }
