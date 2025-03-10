04e9c06e77bf0e1f56548a46ec8cb17fbbcc400f
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-10 11:24:08 +0000

    refactor: simplify RecentChat and SidebarMobile components by removing unused props and integrating state management

--- src/app/chats/[chatId]/(sidebar)/RecentChat.tsx
+++ src/app/chats/[chatId]/(sidebar)/RecentChat.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import PresenceDot from "@/components/PresenceDot";
-import MemberImage from "@/components/MemberImage";
 import RecentChatFrame from "./RecentChatFrame";
 import { useAppSelector } from "@/redux-store/hooks";
 import { IoIosArrowForward } from "react-icons/io";
@@ -10,17 +8,14 @@ import {
 } from "@/redux-store/features/membersSlice";
 import { selectChatById } from "@/redux-store/features/chatsSlice";
 import { selectMessageById } from "@/redux-store/features/messagesSlice";
+import PresenceAvatar from "@/components/PresenceAvatar";
 
 type Props = {
   chatId: string;
-  isSidebarOpen?: boolean;
-  setIsSidebarOpen?: (isOpen: boolean) => void;
 };
 
 export default function RecentChat({
   chatId,
-  isSidebarOpen,
-  setIsSidebarOpen,
 }: Props) {
   const chat = useAppSelector((state) => selectChatById(state, chatId));
   const { chatPartnerId, messageIds = [], unreadMessageCount } = chat;
@@ -34,46 +29,31 @@ export default function RecentChat({
   const online = useAppSelector((state) =>
     selectMemberOnlineStatus(state, chatPartnerId)
   );
-  const trimmedLastMessage =
-    lastMessage && lastMessage.length > 25
-      ? `${lastMessage.slice(0, 25)}...`
-      : lastMessage;
 
   return (
     <>
       {chatPartner && (
         <RecentChatFrame
           chatId={chatId}
-          isSidebarOpen={isSidebarOpen}
-          setIsSidebarOpen={setIsSidebarOpen}
         >
           <div className="flex items-center">
-            <div className="m-2 rounded-full border-2 border-slate-300 dark:border-slate-500 overflow-hidden">
-              <MemberImage
-                memberImage={
-                  chatPartner.image ? chatPartner.image : "/images/user.png"
-                }
-                memberName={chatPartner.name ? chatPartner.name : ""}
-                width={50}
-                height={50}
+            <div className="m-2">
+              <PresenceAvatar
+                src={chatPartner.image || "/images/user.png"}
+                online={online}
+                deleted={chatPartner.deleted || false}
+                imageWidth={50}
+                imageHeight={50}
+                classNames="self-end mx-1"
+                own={false}
               />
-              {online && (
-                <div className="absolute top-9 left-11 z-20">
-                  <PresenceDot />
-                </div>
-              )}
-              {chatPartner.deleted && (
-                <div className="absolute bottom-[2px] left-3 z-20 bg-gray-400 dark:bg-gray-600 px-1 text-[10px] border-1 rounded-full ">
-                  Deleted
-                </div>
-              )}
             </div>
-            <div className="flex flex-col text-gray-800 dark:text-white pr-8">
+            <div className="flex flex-col text-gray-800 dark:text-white pr-1 max-w-[70%]">
               <div>
                 {chatPartner.deleted ? "Deleted User" : chatPartner.name}
               </div>
-              <div className="text-xs text-gray-400 min-w-full">
-                {trimmedLastMessage || "No message to show"}
+              <div className="text-xs text-gray-400 line-clamp-2">
+                {lastMessage || "No message to show"}
                 {unreadMessageCount > 0 && (
                   <div className="absolute bottom-6 right-[30px] sm:right-[10px] bg-secondary rounded-full text-white text-xs w-5 h-5 flex items-center justify-center">
                     {unreadMessageCount}
