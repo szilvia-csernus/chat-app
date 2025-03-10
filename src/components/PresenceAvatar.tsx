8108a75089451c050e91130d503114a11e5539cf
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-10 11:28:11 +0000

    refactor: update PresenceAvatar component to use optional props for online, deleted, classNames, imageWidth, and imageHeight

--- src/components/PresenceAvatar.tsx
+++ src/components/PresenceAvatar.tsx
@@ -1,12 +1,14 @@
-import { Avatar, Badge } from "@heroui/react";
+import { Badge } from "@heroui/react";
 import React from "react";
 import MemberImage from "./MemberImage";
 
 type Props = {
   src?: string;
-  online: boolean;
-  deleted: boolean;
-  className?: string;
+  online?: boolean;
+  deleted?: boolean;
+  classNames?: string;
+  imageWidth?: number;
+  imageHeight?: number;
   own?: boolean;
 };
 
@@ -14,10 +16,13 @@ export default function PresenceAvatar({
   src,
   online,
   deleted,
+  classNames,
+  imageWidth,
+  imageHeight,
   own,
 }: Props) {
   return (
-    <div className="relative flex items-end">
+    <div className={`relative flex justify-center items-end text-xs ${classNames}`}>
       <Badge
         content=""
         color="success"
@@ -25,16 +30,18 @@ export default function PresenceAvatar({
         placement="bottom-right"
         isInvisible={!online || deleted || own}
       >
-        <MemberImage
-          memberImage={src || "/images/user.png"}
-          memberName="User avatar"
-          width={40}
-          height={40}
-          className="h-[40px] border-1 border-slate-300 dark:border-slate-500 rounded-full z-5"
-        />
+        <div className="1">
+          <MemberImage
+            memberImage={src || "/images/user.png"}
+            memberName="User avatar"
+            imageWidth={imageWidth}
+            imageHeight={imageHeight}
+            className="border-1 border-slate-300 dark:border-slate-500 rounded-full z-5"
+          />
+        </div>
       </Badge>
       {deleted && (
-        <div className="absolute bottom-[-9px] bg-gray-400 dark:bg-gray-600 px-1 text-[10px] border-1 rounded-full ">
+        <div className="absolute bottom-[-7px] bg-gray-400 dark:bg-gray-600 px-1 py-0 text-[10px] border-1 rounded-full ">
           Deleted
         </div>
       )}
