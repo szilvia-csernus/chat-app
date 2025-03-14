6b25aabe4dcc3c617618c6cc31b9227945d2cbb5
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-14 10:01:51 +0000

    refactor: update MessageGroup component to use message clusters for improved message organization

--- src/app/chats/[chatId]/(main)/MessageGroup.tsx
+++ src/app/chats/[chatId]/(main)/MessageGroup.tsx
@@ -1,37 +1,36 @@
 "use client";
 
-import React, { useRef } from "react";
-import MessageBox from "./MessageBox";
-
 import { timeAgoDate } from "@/lib/utils";
 import { useAppSelector } from "@/redux-store/hooks";
-import { selectCurrentChatGroupedMessageIdsByDate } from "@/redux-store/features/chatsSlice";
+import { selectCurrentChatMsgClustersDataByDate } from "@/redux-store/features/chatsSlice";
+import MessageCluster from "./MessageCluster";
 
 type Props = {
   date: string;
 };
 
-export default function MessageGroup({ date}: Props) {
-  const messageIdGroup = useAppSelector((state) => selectCurrentChatGroupedMessageIdsByDate(state, date));
+export default function MessageGroup({ date }: Props) {
+  const messageClustersData = useAppSelector((state) =>
+    selectCurrentChatMsgClustersDataByDate(state, date)
+  );
 
-  const lastMsgSender = useRef<"currentMember" | "chatPartner" | null>(null);
+  const msgClusterIds = messageClustersData?.clusterIds || [];
 
   return (
     <>
-      {date && <div
-        suppressHydrationWarning={true}
-        className=" w-none min-w-32 max-w-48 text-center text-xs font-semibold my-4 mx-auto py-1 px-2 border-1  text-secondary dark:text-teal-300 rounded-full border-slate-300 dark:border-slate-500"
-      >
-        {timeAgoDate(date)}
-      </div>}
-      {/* message group for specific date */}
+      {date && (
+        <div
+          suppressHydrationWarning={true}
+          className=" w-none min-w-32 max-w-48 text-center text-xs font-semibold mt-6 mb-4 mx-auto py-1 px-2 border-1  text-secondary dark:text-teal-300 rounded-full border-slate-300 dark:border-slate-500"
+        >
+          {timeAgoDate(date)}
+        </div>
+      )}
+      {/* message clusters for specific date */}
       <ul>
-        {messageIdGroup.map((id) => (
+        {msgClusterIds.map((id) => (
           <li key={id}>
-            <MessageBox
-              messageId={id}
-              lastMsgSender={lastMsgSender}
-            />
+            <MessageCluster clusterId={id} date={date} />
           </li>
         ))}
       </ul>
