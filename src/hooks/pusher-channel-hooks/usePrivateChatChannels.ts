7253ada56b05f2d766d8fc1c6c7b83e376280972
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-14 17:06:28 +0000

    refactor: simplify message handling in usePrivateChatChannels by integrating addNewMessage thunk

--- src/hooks/pusher-channel-hooks/usePrivateChatChannels.ts
+++ src/hooks/pusher-channel-hooks/usePrivateChatChannels.ts
@@ -4,16 +4,12 @@ import { useEffect, useRef } from "react";
 import { Channel } from "pusher-js";
 import { pusherClient } from "@/lib/pusher";
 import { useCallback } from "react";
-import { updateReadStatus } from "@/app/actions/messageActions";
-import { getUnreadMessageCount } from "@/app/actions/chatActions";
 import { CurrentMember, SerializedMessage } from "@/types";
-import { useParams } from "next/navigation";
 import { AppStore } from "@/redux-store/store";
-import { addMsgId, updateUnreadCount } from "@/redux-store/features/chatsSlice";
 import {
-  addNewMsg,
   updateMsgReadStatus,
 } from "@/redux-store/features/messagesSlice";
+import { addNewMessage } from "@/redux-store/thunks";
 
 type Props = {
   store: AppStore;
@@ -26,62 +22,15 @@ export const usePrivateChatChannels = ({ store, currentMember }: Props) => {
   // Ref is used to prevent the creation of multiple channels when the component re-renders
   const chatChannelRefs = useRef<{ [key: string]: Channel | null }>({});
 
-  // storing the active chatid in a ref to be used in the handleNewMessage function
-  // which otherwise would not have access to it when
-  // triggered by the channel event
-  const currentChatRef = useRef<string | null>(null);
-  const params = useParams<{ chatId: string }>();
-  currentChatRef.current = params.chatId;
-  // console.log("Active chat in usePrivateChatChannels", currentChatRef.current);
-
   const currentMemberId = currentMember?.id;
-
-  // at this point, we don't have access to the "useAppSelector" hook yet
-  // so we need to get the chat ids from the store directly
-  const chats = store.getState().chats.chats;
-  const chatIds = Object.keys(chats);
-  const activeChatIds = chatIds.filter((id) => !chats[id].inactive);
+  
 
   const handleNewMessage = useCallback(
-    async (chatId: string, message: SerializedMessage, date: string) => {
-      console.log("Handling new message", chatId, currentChatRef.current);
-      // If the message is for the acctive chat, add it to the chat
-      // on either or both the sender and receiver side
-      if (currentChatRef.current === chatId) {
-        console.log("Adding message to active chat...", currentChatRef.current);
-        store.dispatch(addNewMsg(message));
-        store.dispatch(
-          addMsgId({
-            chatId,
-            senderId: message.senderId!,
-            messageId: message.id,
-            date,
-          })
-        );
-      }
-
-      // Receiver side: if the message is not for the active chat,
-      // update the unread count for that chat
-      if (
-        currentChatRef.current !== chatId &&
-        message.senderId !== currentMemberId
-      ) {
-        const unreadCount = await getUnreadMessageCount(chatId);
-        store.dispatch(updateUnreadCount({ chatId, count: unreadCount }));
-      }
-
-      // Receiver side: if the message is for the active chat,
-      // update the read status of the message
-      if (
-        currentChatRef.current === chatId &&
-        message.senderId !== currentMemberId
-      ) {
-        // console.log("New message received for chat: ", chatId, message);
-        // console.log("Active chat is: ", currentChatRef.current);
-        await updateReadStatus(message.id);
-      }
+    (chatId: string, message: SerializedMessage, date: string) => {
+      console.log("Handling new message", chatId);
+      store.dispatch(addNewMessage(chatId, message, date))
     },
-    [currentMemberId, store]
+    []
   );
 
   const handleMessageRead = useCallback(
@@ -93,6 +42,12 @@ export const usePrivateChatChannels = ({ store, currentMember }: Props) => {
 
   useEffect(() => {
     if (!currentMemberId) return;
+
+    // at this point, we don't have access to the "useAppSelector" hook yet
+    // so we need to get the chat ids from the store directly
+    const chats = store.getState().chats.chats;
+    const chatIds = Object.keys(chats);
+    const activeChatIds = chatIds.filter((id) => !chats[id].inactive);
 
     // Subscribe to channels for each chat
     activeChatIds.forEach((id) => {
@@ -133,7 +88,7 @@ export const usePrivateChatChannels = ({ store, currentMember }: Props) => {
         }
       });
     };
-  }, [currentMemberId, activeChatIds, handleNewMessage, handleMessageRead]);
+  }, [currentMemberId, handleNewMessage, handleMessageRead]);
 
   return chatChannelRefs.current;
 };
