c01c34d3554174adbef31eb3be9af93c908e79c7
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-14 17:04:28 +0000

    refactor: update RecentChatFrame styles for improved dark mode consistency

--- src/app/chats/[chatId]/(sidebar)/RecentChatFrame.tsx
+++ src/app/chats/[chatId]/(sidebar)/RecentChatFrame.tsx
@@ -17,12 +17,12 @@ export default function RecentChatFrame({ chatId, children }: Props) {
   const dispatch = useAppDispatch();
 
   const additionalStyles =
-    currentChatId === chatId ? "bg-white dark:bg-gray-800" : "";
+    currentChatId === chatId ? "bg-white dark:bg-gray-700" : "";
   
 
   return (
     <Link
-      className={`relative flex flex-row justify-between items-center h-18 rounded-none cursor-pointer border-b-1 border-slate-300 dark:border-slate-500  ${additionalStyles}`}
+      className={`relative flex flex-row justify-between items-center h-18 rounded-none cursor-pointer border-b-1 border-slate-300 dark:border-slate-700  ${additionalStyles}`}
       href={`/chats/${chatId}`}
       onClick={() => dispatch(closeSidebar())}
     >
