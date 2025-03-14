7b77eebd84cc0ea2367b1e947d5f3bd59984e932
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-14 17:07:55 +0000

    refactor: replace Skeleton with Spinner in Loading components for consistent loading indication

--- src/app/profile/complete-profile/loading.tsx
+++ src/app/profile/complete-profile/loading.tsx
@@ -4,7 +4,7 @@ import { Spinner } from "@heroui/react";
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center mt-10">
       <Spinner size="lg" />
     </div>
   );
