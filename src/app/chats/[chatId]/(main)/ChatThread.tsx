49f839dd3e65d0f4fa8340e549134445781d4cbe
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-19 14:12:42 +0000

    fix: update unread message count logic in ChatThread component

--- src/app/chats/[chatId]/(main)/ChatThread.tsx
+++ src/app/chats/[chatId]/(main)/ChatThread.tsx
@@ -8,9 +8,9 @@ import {
   selectCurrentChat,
   selectCurrentChatMsgGroupChronList,
   selectCurrentChatPartnerId,
-  updateUnreadCount,
 } from "@/redux-store/features/chatsSlice";
 import MessageGroup from "./MessageGroup";
+import { updateUnreadMsgCount } from "@/redux-store/thunks";
 
 export default function ChatThread() {
   const dispatch = useAppDispatch();
@@ -20,42 +20,42 @@ export default function ChatThread() {
   const chatPartner = useAppSelector((state) =>
     selectMemberById(state, chatPartnerId)
   );
-  const messageGroupList = useAppSelector(selectCurrentChatMsgGroupChronList);
+  const messageGroupList = useAppSelector((state) =>
+    selectCurrentChatMsgGroupChronList(state.chats)
+  );
 
   useEffect(() => {
     if (currentChat) {
-      // The user just opened this chat, meaning they seen all previously
-      // unread messages, so reset the unread count in the redux store.
-      // For the database, the read status is updated in the backend in
-      // page.tsx
-      dispatch(updateUnreadCount({ chatId: currentChat.id, count: 0 }));
+      // The user just opened this chat, meaning they can now see all previously
+      // unread messages, therefore, we reset the unread count in the redux store.
+      dispatch(updateUnreadMsgCount({ chatId: currentChat.id, count: 0 }));
     }
   });
 
   return (
-      <ul className="flex flex-col px-1 py-2">
-        {currentChat &&
-          chatPartner &&
-          messageGroupList &&
-          messageGroupList.length > 0 && (
-            <>
-              {messageGroupList.map((date: string) => {
-                return (
-                  <li key={date}>
-                    <MessageGroup date={date} />
-                  </li>
-                );
-              })}
-            </>
-          )}
-        {currentChat &&
-          chatPartner &&
-          messageGroupList &&
-          messageGroupList.length === 0 && (
-            <li key={"No message"} className="my-4 pl-1">
-              Start chatting{chatPartner.name && ` with ${chatPartner.name}`}!
-            </li>
-          )}
-      </ul>
+    <ul className="flex flex-col px-1 py-2">
+      {currentChat &&
+        chatPartner &&
+        messageGroupList &&
+        messageGroupList.length > 0 && (
+          <>
+            {messageGroupList.map((date: string) => {
+              return (
+                <li key={date}>
+                  <MessageGroup date={date} />
+                </li>
+              );
+            })}
+          </>
+        )}
+      {currentChat &&
+        chatPartner &&
+        messageGroupList &&
+        messageGroupList.length === 0 && (
+          <li key={"No message"} className="my-4 pl-1">
+            Start chatting{chatPartner.name && ` with ${chatPartner.name}`}!
+          </li>
+        )}
+    </ul>
   );
 }
