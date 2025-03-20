f7e2fd6a3c251c2023534569bb2ea359db50b2ef
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-20 14:06:26 +0000

    refactor: replace badge prop with unreadCount in NavLink component for better readability

--- src/components/navbar/NavLink.tsx
+++ src/components/navbar/NavLink.tsx
@@ -4,15 +4,16 @@ import { NavbarItem } from "@heroui/navbar";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import UnreadCount from "../UnreadCount";
 
 type NavLinkProps = {
   href: string;
   label: string;
-  badge?: number | null;
+  unreadCount?: number | null;
   onClick?: () => void;
 };
 
-export default function NavLink({ href, label, badge, onClick }: NavLinkProps) {
+export default function NavLink({ href, label, unreadCount, onClick }: NavLinkProps) {
   const pathname = usePathname();
   const [isActive, setIsActive] = useState(pathname === href);
 
@@ -31,10 +32,11 @@ export default function NavLink({ href, label, badge, onClick }: NavLinkProps) {
       >
         {label}
       </NavbarItem>
-      {badge && (
-        <div className="absolute top-[2px] right-[-25px] bg-accent rounded-full text-slate-700 font-bold text-xs w-5 h-5 flex items-center justify-center">
-          {badge}
-        </div>
+      {unreadCount && (
+        <UnreadCount
+          unreadCount={unreadCount}
+          className="absolute top-[2px] right-[-25px]"
+        />
       )}
     </div>
   );
