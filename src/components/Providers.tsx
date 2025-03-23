a06b5239ad1f1da02731355c701205e866c641b2
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-23 12:05:13 +0000

    feat: integrate InitialStore component into RootLayout for improved state management

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { CurrentMember, RawChatData } from "@/types";
 import { HeroUIProvider } from "@heroui/react";
 import React, { ReactNode } from "react";
 import { useRouter } from "next/navigation";
@@ -8,25 +7,16 @@ import StoreProvider from "./StoreProvider";
 
 type Props = {
   children: ReactNode;
-  currentMember: CurrentMember | null;
-  recentChats: RawChatData[] | null;
-  currentChat: RawChatData | null;
 };
 
 export default function Providers({
   children,
-  currentMember,
-  recentChats,
-  currentChat,
+
 }: Props) {
   const router = useRouter();
 
   return (
-    <StoreProvider
-      currentMember={currentMember}
-      recentChats={recentChats}
-      currentChat={currentChat}
-    >
+    <StoreProvider>
       <HeroUIProvider navigate={router.push}>{children}</HeroUIProvider>
     </StoreProvider>
   );
