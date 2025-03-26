5411543df5a3f7f1c51c13113bfb8abb8f470dca
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-26 19:20:50 +0000

    refactor: update Chat component to use chatId prop and implement message loading with PullableSpace

--- src/app/chats/[chatId]/(main)/Chat.tsx
+++ src/app/chats/[chatId]/(main)/Chat.tsx
@@ -1,52 +1,64 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Card } from "@heroui/card";
 import ChatThread from "./ChatThread";
 import ChatForm from "./ChatForm";
 import CurrentChatPartner from "./CurrentChatPartner";
-import { RawChatData } from "@/types";
-import { useAppDispatch } from "@/redux-store/hooks";
-import { setCurrentChat } from "@/redux-store/features/chatsSlice";
-import { mapRawChatDataToChatAndMessages } from "@/lib/maps";
-import { setMessages } from "@/redux-store/features/messagesSlice";
-import { setChatVisible } from "@/redux-store/features/uiSlice";
+import { useAppDispatch, useAppSelector } from "@/redux-store/hooks";
+import {
+  selectAllMsgsLoadedForCurrentChat,
+  selectChatsPopulated,
+  selectCurrentChat,
+  selectFirstLoadedMsgIdByChatId,
+  setCurrentChatId,
+} from "@/redux-store/features/chatsSlice";
+import {
+  fetchDataAndPopulateStore,
+  loadMoreMessages,
+} from "@/redux-store/thunks";
+import PullableSpace from "@/components/PullableSpace";
 
-type Props = {
-  initialChat: RawChatData | null;
-};
 
-export default function Chat({ initialChat }: Props) {
+export default function Chat({ chatId }: { chatId: string }) {
+  const populated = useAppSelector(selectChatsPopulated);
   const dispatch = useAppDispatch();
-  const initialized = useRef(false);
+
+  const currentChat = useAppSelector(state => selectCurrentChat(state.chats));
+  const firstLoadedMsgId = useAppSelector((state) =>
+    selectFirstLoadedMsgIdByChatId(state, currentChat?.id || null)
+  );
+  const [cursor, setCursor] = useState<string | null>(firstLoadedMsgId);
+  const allMessagesLoaded = useAppSelector(selectAllMsgsLoadedForCurrentChat);
+
+  // used for the first time the chat is opened (if user was not originally
+  // signed in when the app first loaded and this is the first page they visit)
+  useEffect(() => {
+    dispatch(setCurrentChatId(chatId));
+    if (!populated) {
+      dispatch(fetchDataAndPopulateStore());
+    }
+  }, [dispatch, populated, chatId]);
+
+  const handlePull = () => {
+    if (allMessagesLoaded || !cursor || !currentChat) return;
+    dispatch(loadMoreMessages(currentChat.id, cursor)); // Fetch more messages when scrolled to the top
+  };
 
   useEffect(() => {
-    if (!initialized.current && initialChat) {
-      console.log(
-        "Chat useEffect: Setting initial chat in store",
-        initialChat.id
-      );
-      const result = mapRawChatDataToChatAndMessages(initialChat);
-      if (result) {
-        const { chat, messages } = result;
-        dispatch(setCurrentChat(chat));
-        dispatch(setMessages(messages));
-        initialized.current = true;
-      }
-    }
-  }, [initialChat, dispatch]);
+    setCursor(firstLoadedMsgId);
+  }, [firstLoadedMsgId]);
 
   const messageEndRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    dispatch(setChatVisible(true));
-  });
-
-  useEffect(() => {
+  function jumpToBottom() {
     if (messageEndRef.current) {
       messageEndRef.current.scrollIntoView({ behavior: "auto" });
     }
-  }, [messageEndRef]);
+  }
+
+  useEffect(() => {
+    jumpToBottom();
+  }, []);
 
   return (
     <Card
@@ -56,17 +68,30 @@ export default function Chat({ initialChat }: Props) {
       <div className="sticky space-x-2 sm:hidden">
         <CurrentChatPartner />
       </div>
-      <div className="flex flex-col-reverse h-svh overflow-scroll scrollbar-hide scroll-smooth">
-        <ChatThread />
+      <div className="flex flex-col-reverse h-svh overflow-y-scroll overscroll-y-contain touch-pan-y scrollbar-hide scroll-smooth">
+        {/* Pullable space */}
+        <PullableSpace
+          onPull={handlePull}
+          allMessagesLoaded={allMessagesLoaded}
+        >
+          {allMessagesLoaded && (
+            <div className="text-center text-xs pt-4">
+              This is the start of the conversation
+            </div>
+          )}
+          <ChatThread />
+          {/* Ref to mark the end of the conversation */}
+          <div ref={messageEndRef} />
+        </PullableSpace>
+        {/* Chat thread */}
       </div>
       <div className="sticky mb-3">
-        {initialChat?.inactive && (
+        {currentChat?.inactive && (
           <div className="bg-gray-300 dark:bg-gray-700 px-2 py-3 text-center text-sm">
             This chat is inactive. Your chat partner has deleted their account.
           </div>
         )}
-        <div ref={messageEndRef} />
-        {!initialChat?.inactive && <ChatForm />}
+        {!currentChat?.inactive && <ChatForm jumpToBottom={jumpToBottom} />}
       </div>
     </Card>
   );
