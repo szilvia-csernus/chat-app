1b8ac65cca0aa4cef70ef307627440c7b32de79d
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-27 12:29:57 +0000

    refactor: remove unused currentChat prop and debug console log from InitialStore component

--- src/components/InitialStore.tsx
+++ src/components/InitialStore.tsx
@@ -8,31 +8,22 @@ type Props = {
   currentMember: CurrentMember | null;
   membersData: ProfileData[] | null;
   recentChats: RawChatData[] | null;
-  currentChat: RawChatData | null;
 };
 
 export default function InitialStore({
   currentMember,
   membersData,
   recentChats,
-  currentChat,
 }: Props) {
   const initialized = useRef(false);
 
   if (!initialized.current) {
-    console.log("InitialStore: Setting initial store data", 
-      "currentMember", !!currentMember,
-      "recentChats", !!recentChats,
-      "currentChat", !!currentChat
-    );
-
     if (!currentMember) return;
 
     usePopulateStore({
       currentMember,
       membersData,
       recentChats,
-      currentChat,
     });
 
     initialized.current = true;
