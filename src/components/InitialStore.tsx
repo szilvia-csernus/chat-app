72dd60c5ce1101337a7d6bef0f406e026f368dfe
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-03-28 15:50:08 +0000

    feat: refactor InitialStore component to improve initialization logic

--- src/components/InitialStore.tsx
+++ src/components/InitialStore.tsx
@@ -1,8 +1,15 @@
 "use client";
 import { useRef } from "react";
 import { CurrentMember, ProfileData, RawChatData } from "@/types";
-import { usePopulateStore } from "@/redux-store/hooks";
-
+import { useAppDispatch} from "@/redux-store/hooks";
+import { setCurrentMember } from "@/redux-store/features/currentMemberSlice";
+import {
+  mapProfilesDataToMembers,
+  mapRawChatDataListToChatsAndMessages,
+} from "@/lib/maps";
+import { setMembers } from "@/redux-store/features/membersSlice";
+import { setChats, setCurrentChatId } from "@/redux-store/features/chatsSlice";
+import { setMessages } from "@/redux-store/features/messagesSlice";
 
 type Props = {
   currentMember: CurrentMember | null;
@@ -16,19 +23,31 @@ export default function InitialStore({
   recentChats,
 }: Props) {
   const initialized = useRef(false);
+  const dispatch = useAppDispatch();
 
-  if (!initialized.current) {
-    if (!currentMember) return;
+  if (initialized.current || !currentMember) return null;
 
-    usePopulateStore({
-      currentMember,
-      membersData,
-      recentChats,
-    });
+  dispatch(setCurrentMember(currentMember));
 
-    initialized.current = true;
+  const members = membersData && mapProfilesDataToMembers(membersData);
+  if (members) {
+    dispatch(setMembers(members));
   }
+
+  if (!recentChats) return;
+
+  const { chats, messages } = mapRawChatDataListToChatsAndMessages(recentChats);
+
+  dispatch(setChats(chats));
+  dispatch(setMessages(messages));
+
+  const lastChatId = currentMember.lastActiveConversationId;
+
+  if (lastChatId) {
+    dispatch(setCurrentChatId(lastChatId));
+  }
+
+  initialized.current = true;
 
   return null;
 }
-
