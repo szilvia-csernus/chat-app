ec184086736dfd6349fcda6522e6e09a484bef08
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-04-07 10:26:48 +0100

    feat: integrate ContactMe component into Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
+import ContactMe from "./ContactMe";
 
 export default function Footer() {
 
 
   return (
-    <div className="flex h-[80px] bg-gradient-to-r from-slate-700 to-teal-700 p-2 w-full max-w-4xl mx-auto justify-center items-center text-white">
+    <div className="flex h-[80px] bg-gradient-to-r from-slate-700 to-teal-700 gap-0 w-full max-w-4xl mx-auto justify-center items-center text-white">
       <p>
-        Got questions? Contact me <span className="text-accent underline cursor-pointer">here</span>
+        Got questions? <ContactMe />
       </p>
     </div>
   );
