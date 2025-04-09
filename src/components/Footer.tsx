1b1d93f9ad8e4254ffb34d1e59c326380e1c57bf
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-04-09 14:13:49 +0100

    fix: add z-index to Footer component for improved layering

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,8 @@ export default function Footer() {
 
 
   return (
-    <div className="flex h-[80px] bg-gradient-to-r from-slate-700 to-teal-700 gap-0 w-full max-w-4xl mx-auto justify-center items-center text-white">
+    <div className="flex h-[80px] bg-gradient-to-r from-slate-700 to-teal-700 gap-0 
+    w-full max-w-4xl mx-auto justify-center items-center text-white z-10">
       <p>
         Got questions? <ContactMe />
       </p>
