6d6d4646aeedb8acc36c041b211cb98b89b37d80
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-04-10 12:11:43 +0100

    fix: enhance LoadingSpinner layout and include Footer for improved UI consistency

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -2,11 +2,15 @@
 
 import { Spinner } from '@heroui/react';
 import React from 'react'
+import Footer from './Footer';
 
 export default function LoadingSpinner() {
   return (
-    <div className="flex items-center justify-center mt-10">
-      <Spinner size="lg" />
-    </div>
+    <>
+      <div className="h-[calc(100dvh-160px)] w-full flex items-center justify-center bg-zig-zag">
+        <Spinner size="lg" variant="spinner" />
+      </div>
+      <Footer />
+    </>
   );
 }
