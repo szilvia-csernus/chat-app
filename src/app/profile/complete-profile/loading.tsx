e2dd680cf5bd2efee7ec2d55f96f8b8326a7e97c
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-04-14 14:31:28 +0100

    feat: add Footer component to Loading for improved user experience

--- src/app/profile/complete-profile/loading.tsx
+++ src/app/profile/complete-profile/loading.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import Footer from "@/components/Footer";
 import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function Loading() {
   return (
-    <LoadingSpinner />
+    <>
+      <LoadingSpinner />
+      <Footer />
+    </>
   );
 }
