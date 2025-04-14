4b45a76778a70ec103b985bdced832fe16607a01
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-04-14 14:29:42 +0100

    fix: remove Footer from LoadingSpinner component for streamlined layout

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,16 +1,12 @@
-'use client';
+"use client";
 
-import { Spinner } from '@heroui/react';
-import React from 'react'
-import Footer from './Footer';
+import { Spinner } from "@heroui/react";
+import React from "react";
 
 export default function LoadingSpinner() {
   return (
-    <>
-      <div className="h-[calc(100dvh-160px)] w-full flex items-center justify-center bg-zig-zag">
-        <Spinner size="lg" variant="spinner" />
-      </div>
-      <Footer />
-    </>
+    <div className="h-[calc(100dvh-160px)] w-full flex items-center justify-center bg-zig-zag">
+      <Spinner size="lg" variant="spinner" />
+    </div>
   );
 }
