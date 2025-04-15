3e67900484204177024ceae7145502783e33c726
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-04-15 17:04:50 +0100

    feat: improve serialization of profile data to handle lastActive date formats

--- src/lib/serialize.ts
+++ src/lib/serialize.ts
@@ -1,8 +1,14 @@
 // Serialization of data is required by redux
 
-import { ProfileData, Member, SerializedMessage, CurrentMember, CurrentProfileData, MessageData } from "@/types";
+import {
+  ProfileData,
+  Member,
+  SerializedMessage,
+  CurrentMember,
+  CurrentProfileData,
+  MessageData,
+} from "@/types";
 import { formatShortTime } from "./utils";
-
 
 export function serializeMessage(message: MessageData): SerializedMessage {
   return {
@@ -16,16 +22,26 @@ export function serializeMessage(message: MessageData): SerializedMessage {
   };
 }
 
-export function serializeMessages(messages: MessageData[]): SerializedMessage[] {
+export function serializeMessages(
+  messages: MessageData[]
+): SerializedMessage[] {
   return messages.map((message) => serializeMessage(message));
 }
 
 export function serializeProfileDataToMember(profile: ProfileData): Member {
+  // This conversion is neccesary because after page-reloads, the members
+  // data is either fetched from the server (where lasActive is a Date) or
+  // from the redux store (where lastActive is a string).
+  const lastActiveDate =
+    typeof profile.lastActive === "string"
+      ? profile.lastActive
+      : new Date(profile.lastActive).toISOString();
+
   return {
     id: profile.id,
     name: profile.user?.name || "",
     image: profile.user?.image || null,
-    lastActive: (profile.lastActive).toISOString(),
+    lastActive: lastActiveDate,
     deleted: profile.deleted,
     chatting:
       profile.conversations.length > 0 ? profile.conversations[0].id : null,
@@ -33,12 +49,14 @@ export function serializeProfileDataToMember(profile: ProfileData): Member {
   };
 }
 
-export function serializeCurrentProfileDataToCurrentMember(profile: CurrentProfileData): CurrentMember {
+export function serializeCurrentProfileDataToCurrentMember(
+  profile: CurrentProfileData
+): CurrentMember {
   return {
     id: profile.id,
     name: profile.user?.name || "",
     image: profile.user?.image || null,
-    lastActive: (profile.lastActive).toISOString(),
+    lastActive: profile.lastActive.toISOString(),
     deleted: profile.deleted,
     online: false,
     lastActiveConversationId: profile.lastActiveConversationId || null,
