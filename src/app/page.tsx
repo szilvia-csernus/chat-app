e8d52f0cf70fa0f43ac3b070234462e4417a94c7
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-04-17 12:16:53 +0100

    fix: set explicit height of main content area for Safari

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
 
   return (
     <div className="h-full min-h-[calc(100dvh-80px)] w-full flex flex-col justify-stretch items-center">
-      <div className="flex-grow items-center w-full h-1 md:my-2 py-8 px-5 text-center text-slate-600
+      <div className="flex-grow items-center w-full h-[calc(100dvh-160px)] md:my-2 py-8 px-5 text-center text-slate-600
        dark:text-slate-300 md:border-1 border-slate-300 dark:border-slate-700 bg-zig-zag flex 
        flex-col gap-7">
         <h1 className="font-bold text-xl mb-4">
