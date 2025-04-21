8c4ab77d250fb934d2f30ccf9826b87c60397c9e
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-04-21 17:58:15 +0100

    fix: correct formatting of div element in Home component for consistent footer styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,11 @@ export default async function Home() {
 
   return (
     <div className="h-full min-h-[calc(100dvh-80px)] w-full flex flex-col justify-stretch items-center">
-      <div className="flex-grow items-center w-full h-[calc(100dvh-160px)] md:my-2 py-8 px-5 text-center text-slate-600
+      <div
+        className="flex-grow items-center w-full h-[calc(100dvh-160px)] md:h-[calc(100dvh-176px)] md:my-2 py-8 px-5 text-center text-slate-600
        dark:text-slate-300 md:border-1 border-slate-300 dark:border-slate-700 bg-zig-zag flex 
-       flex-col gap-7">
+       flex-col gap-7"
+      >
         <h1 className="font-bold text-xl mb-4">
           Welcome to{" "}
           <span className="text-secondary dark:text-teal-200 text-shadow">
