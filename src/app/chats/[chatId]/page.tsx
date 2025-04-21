98169ec3c6ef478c30026d338ad7d52f6c84ed93
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-04-21 15:25:58 +0100

    feat: pass chat partner's last active status to Chat component

--- src/app/chats/[chatId]/page.tsx
+++ src/app/chats/[chatId]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Chat from "./(main)/Chat";
+import { getChatPartnerLastActiveByChatId } from "@/app/actions/memberActions";
 
 export const dynamic = "force-dynamic";
 
@@ -9,10 +10,11 @@ type Params = {
 
 export default async function ChatsPage({ params }: Params) {
   const { chatId } = await params;
+  const chatPartnerLastActive = await getChatPartnerLastActiveByChatId(chatId);
 
   return (
     <div className="w-full sm:col-span-7 relative">  
-        <Chat chatId={chatId}/>
+        <Chat chatId={chatId} chatPartnerLastActive={chatPartnerLastActive}/>
     </div>
   );
 }
