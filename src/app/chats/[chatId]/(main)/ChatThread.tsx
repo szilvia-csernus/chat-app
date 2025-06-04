b723b03288d47b63b53e0b8932464b893c01e318
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-06-04 16:36:39 +0100

    fix: remove unnecessary fragment in ChatThread component for cleaner rendering

--- src/app/chats/[chatId]/(main)/ChatThread.tsx
+++ src/app/chats/[chatId]/(main)/ChatThread.tsx
@@ -13,7 +13,6 @@ import MessageGroup from "./MessageGroup";
 import { resetUnreadMsgCount } from "@/redux-store/thunks";
 import { setChatVisible } from "@/redux-store/features/uiSlice";
 
-
 export default function ChatThread() {
   const dispatch = useAppDispatch();
   const currentChatId = useAppSelector(selectCurrentChatId);
@@ -38,34 +37,30 @@ export default function ChatThread() {
     dispatch(setChatVisible(true));
   }, [dispatch]);
 
-
   return (
-    <>
-      <ul className="flex flex-col px-1 py-2 scroll-behavior-smooth">
-        {currentChatId &&
-          chatPartner &&
-          messageGroupList &&
-          messageGroupList.length > 0 && (
-            <>
-              {messageGroupList.map((date: string, idx) => {
-                return (
-                  <li key={date}>
-                    <MessageGroup date={date} first={idx === 0}/>
-                  </li>
-                );
-              })}
-            </>
-          )}
-        {currentChatId &&
-          chatPartner &&
-          messageGroupList &&
-          messageGroupList.length === 0 && (
-            <li key={"No message"} className="my-4 pl-1">
-              Start chatting{chatPartner.name && ` with ${chatPartner.name}`}!
-            </li>
-          )}
-      </ul>
-      
-    </>
+    <ul className="flex flex-col px-1 py-2 scroll-behavior-smooth">
+      {currentChatId &&
+        chatPartner &&
+        messageGroupList &&
+        messageGroupList.length > 0 && (
+          <>
+            {messageGroupList.map((date: string, idx) => {
+              return (
+                <li key={date}>
+                  <MessageGroup date={date} first={idx === 0} />
+                </li>
+              );
+            })}
+          </>
+        )}
+      {currentChatId &&
+        chatPartner &&
+        messageGroupList &&
+        messageGroupList.length === 0 && (
+          <li key={"No message"} className="my-4 pl-1">
+            Start chatting{chatPartner.name && ` with ${chatPartner.name}`}!
+          </li>
+        )}
+    </ul>
   );
 }
