ab841b70ecc30d346b992f83b6f983e478693bf4
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-06-04 16:36:27 +0100

    fix: update layout styles in LoginPage for improved responsiveness

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,9 +4,9 @@ import Footer from "@/components/Footer";
 
 export default async function LoginPage() {
   return (
-    <div className="w-full h-full flex flex-col justify-strech">
+    <div className="w-full min-h-[calc(100dvh-80px)] flex flex-col justify-strech">
       <div
-        className="h-full min-h-[calc(100dvh-160px)] md:border-1 border-slate-300 dark:border-slate-700 
+        className="min-h-[calc(100dvh-160px)] md:min-h-[calc(100dvh-176px)] md:border-1 border-slate-300 dark:border-slate-800 
       bg-zig-zag flex flex-grow w-full md:my-2 items-center justify-center vertical-center"
       >
         <LoginForm />
