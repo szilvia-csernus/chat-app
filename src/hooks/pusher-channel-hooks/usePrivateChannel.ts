00023b107af966311596b75347c85941410b0303
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2025-06-05 13:40:49 +0100

    fix: refactor usePrivateChannel to remove console.log statements and streamline channel management

--- src/hooks/pusher-channel-hooks/usePrivateChannel.ts
+++ src/hooks/pusher-channel-hooks/usePrivateChannel.ts
@@ -20,17 +20,15 @@ import { formatShortDateTime } from "@/lib/utils";
 
 // Singleton constant is used for the channel to prevent the creation of 
 // multiple channels when the component re-renders (useRef would be equally valid)
-const privateChannelRef: { [key: string]: Channel | null } = {};
+let privateChannel: Channel | null = null;
 
 export const usePrivateChannel = () => {
-  console.log("Private");
 
   const dispatch = useAppDispatch();
   const currentMemberId = useAppSelector(selectCurrentMemberId);
 
   const handleNewChat = useCallback(
     (data: { newChat: ChatData; chatPartnerId: string }) => {
-      console.log("usePrivateChannel: New chat received", data.newChat.id);
       dispatch(addNewChat(data.newChat));
       // add new chat partner
       dispatch(
@@ -45,7 +43,6 @@ export const usePrivateChannel = () => {
 
   const handleChatInactive = useCallback(
     (chatId: string, messageIds: string[]) => {
-      console.log("usePrivateChannel: handleChatInactive", chatId);
       dispatch(deactivateChat(chatId));
       dispatch(updateMsgsWithDeletedStatus(messageIds));
     },
@@ -55,7 +52,6 @@ export const usePrivateChannel = () => {
   const handleNewMember = useCallback(
     async (newMember: Member) => {
       // add newly-signed-up member to redux store on other people's devices
-      console.log("usePrivateChannel: New member data", newMember);
       if (newMember) {
         console.log(
           "usePrivateChannel: Dispatching addMember for new member",
@@ -82,12 +78,7 @@ export const usePrivateChannel = () => {
         ...member,
         lastActive: lastActive,
       };
-      console.log("usePrivateChannel: Updating member data", memberWithLocalTime);
       if (memberWithLocalTime) {
-        console.log(
-          "usePrivateChannel: Dispatching updateMember for updated member",
-          memberWithLocalTime
-        );
         dispatch(updateMember(memberWithLocalTime));
       }
     },
@@ -98,7 +89,6 @@ export const usePrivateChannel = () => {
   const handleDeleteMember = useCallback(
     async (memberId: string) => {
       // remove member from redux store on other people's devices
-      console.log("usePrivateChannel: Deleting member", memberId);
       dispatch(updateMemberWithDeletedStatus(memberId));
     },
     [dispatch]
@@ -106,15 +96,10 @@ export const usePrivateChannel = () => {
 
   useEffect(() => {
     if (currentMemberId) {
-      console.log(
-        "usePrivateChannel: Subscribing to private channel",
-        `private-${currentMemberId}`
-      );
-      if (!privateChannelRef["presence"]) {
-        privateChannelRef["presence"] = pusherClient.subscribe(
+      if (!privateChannel) {
+        privateChannel = pusherClient.subscribe(
           `private-${currentMemberId}`
         );
-        const privateChannel = privateChannelRef["presence"];
         privateChannel.bind(
           "new-chat",
           (data: { newChat: ChatData; chatPartnerId: string }) => {
@@ -124,32 +109,16 @@ export const usePrivateChannel = () => {
         privateChannel.bind(
           "chat-inactive",
           (data: { chatId: string; messageIds: string[] }) => {
-            console.log(
-              "usePrivateChannel: Chat inactive event received",
-              data.chatId
-            );
             handleChatInactive(data.chatId, data.messageIds);
           }
         );
         privateChannel.bind("new-member", (data: { newMember: Member }) => {
-          console.log(
-            "usePrivateChannel: New member event received",
-            data.newMember
-          );
           handleNewMember(data.newMember);
         });
         privateChannel.bind("update-member", (data: { member: Member }) => {
-          console.log(
-            "usePrivateChannel: Update member event received",
-            data.member
-          );
           handleUpdateMember(data.member);
         });
         privateChannel.bind("delete-member", (data: { memberId: string }) => {
-          console.log(
-            "usePrivateChannel: Member deleted event received",
-            data.memberId
-          );
           handleDeleteMember(data.memberId);
         });
       }
@@ -157,14 +126,10 @@ export const usePrivateChannel = () => {
 
     // Cleanup function to unsubscribe from channels
     return () => {
-      if (privateChannelRef["presence"]) {
-        console.log(
-          "usePrivateChannel: Unsubscribing from private channel",
-          `private-${currentMemberId}`
-        );
-        privateChannelRef["presence"].unbind();
+      if (currentMemberId && privateChannel) {
+        privateChannel.unbind();
         pusherClient.unsubscribe(`private-${currentMemberId}`);
-        privateChannelRef["presence"] = null;
+        privateChannel = null;
       }
     };
   }, [
